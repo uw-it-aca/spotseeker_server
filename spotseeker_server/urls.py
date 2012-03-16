31ba77175853cb0784a16a18c50196af69a953cf
Patrick Michaud <pmichaud@uw.edu>
2012-03-16 16:23:58 -0700

    Allowing a / at the end of the spot search url

--- spotseeker_server/urls.py
+++ spotseeker_server/urls.py
@@ -7,7 +7,7 @@ from spotseeker_server.views.thumbnail import ThumbnailView
 
 urlpatterns = patterns('',
     url(r'v1/spot/(?P<spot_id>\d+)$', SpotView().run),
-    url(r'v1/spot$', SearchView().run),
+    url(r'v1/spot/?$', SearchView().run),
     url(r'v1/spot/(?P<spot_id>\d+)/image$', AddImageView().run),
     url(r'v1/spot/(?P<spot_id>\d+)/image/(?P<image_id>\d+)$', ImageView().run),
     url(r'v1/spot/(?P<spot_id>\d+)/image/(?P<image_id>\d+)/thumb/(?P<thumb_width>\d+)x(?P<thumb_height>\d+)$', ThumbnailView().run),

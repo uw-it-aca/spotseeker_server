42ea977a4275fd51674e43377bab9c7443432c80
Patrick Michaud <pmichaud@uw.edu>
2012-03-19 16:56:21 -0700

    Adding a 404 for bad GET urls

--- spotseeker_server/views/spot.py
+++ spotseeker_server/views/spot.py
@@ -1,5 +1,6 @@
 from spotseeker_server.views.rest_dispatch import RESTDispatch
 from spotseeker_server.forms.spot import SpotForm
+from spotseeker_server.models import Spot
 from django.http import HttpResponse
 from spotseeker_server.require_auth import *
 import simplejson as json
@@ -7,7 +8,14 @@ import simplejson as json
 class SpotView(RESTDispatch):
     @app_auth_required
     def GET(self, request, spot_id):
-        return HttpResponse("This is the RESTy view for spot id: "+spot_id)
+        try:
+            spot = Spot.objects.get(pk=spot_id)
+            return HttpResponse("This is the RESTy view for spot id: "+spot_id)
+        except:
+            response = HttpResponse("Spot not found")
+            response.status_code = 404
+            return response
+
 
     @user_auth_required
     def PUT(self, request, spot_id):

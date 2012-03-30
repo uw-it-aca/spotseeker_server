a0f15f5a4a6ca41ad71fec6955a4d0886408ba5a
Craig M. Stimmel <cstimmel@uw.edu>
2012-03-30 07:58:28 -0700

    Added spot GET test with a valid id.

--- spotseeker_server/test/spot_get.py
+++ spotseeker_server/test/spot_get.py
@@ -1,6 +1,7 @@
 from django.utils import unittest
 from django.test.client import Client
 from spotseeker_server.models import Spot
+import simplejson as json
 
 class SpotGETTest(unittest.TestCase):
     def setUp(self):
@@ -25,3 +26,12 @@ class SpotGETTest(unittest.TestCase):
         response = c.get(url, {'bad_param':'does not exist'},)
         self.assertEquals(response.status_code, 200, "Accepts a query string")
         self.assertEquals(response.content, '[]', "Should return empty json")
+
+    def test_valid_id(self):
+        c = Client()
+        url = "/api/v1/spot/%s" % self.spot.pk
+        response = c.get(url)
+        spot_dict = json.loads(response.content)
+        returned_spot = Spot.objects.get(pk=spot_dict['id'])
+        self.assertEquals(response.status_code, 200, "Accepts a valid id")
+        self.assertEquals(returned_spot, self.spot, "Returns the correct spot")

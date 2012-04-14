168b00ff78cd04eaeab07b6302962c95d0310a35
Craig M. Stimmel <cstimmel@uw.edu>
2012-04-14 10:06:36 -0700

    Added SpotImage delete test that should succeed in deleting the image.

--- spotseeker_server/test/images/delete.py
+++ spotseeker_server/test/images/delete.py
@@ -55,18 +55,88 @@ class SpotImageDELETETest(unittest.TestCase):
 
     def test_invalid_id_too_high(self):
         c = Client()
+
+        #GIF
         test_gif_id = self.gif.pk + 10000
         test_url = "/api/v1/spot/%s/image/%s" % (self.url, test_gif_id)
         response = c.delete(test_url)
         self.assertEquals(response.status_code, 404, "Rejects a not-yet existant url")
 
+        #JPEG
         test_jpeg_id = self.jpeg.pk + 10000
         test_url = "/api/v1/spot/%s/image/%s" % (self.url, test_jpeg_id)
         response = c.delete(test_url)
         self.assertEquals(response.status_code, 404, "Rejects a not-yet existant url")
 
+        #PNG
         test_png_id = self.png.pk + 10000
         test_url = "/api/v1/spot/%s/image/%s" % (self.url, test_png_id)
         response = c.delete(test_url)
         self.assertEquals(response.status_code, 404, "Rejects a not-yet existant url")
+
+    def test_actual_delete_with_etag(self):
+        c = Client()
+
+        #GIF
+        response = c.get(self.gif_url)
+        etag = response["ETag"]
+
+        response = c.delete(self.gif_url, If_Match=etag)
+
+        self.assertEquals(response.status_code, 410, "Gives a GONE in response to a valid delete")
+
+        response = c.get(self.gif_url)
+        self.assertEquals(response.status_code, 404, "Gives a 404 on GET after a delete")
+
+        response = c.delete(self.gif_url)
+        self.assertEquals(response.status_code, 404, "Gives a 404 on DELETE after a delete")
+
+        try:
+            test_gif= SpotImage.objects.get(pk=self.gif.pk)
+        except Exception as e:
+            test_gif= None
+
+        self.assertIsNone(test_gif, "Can't objects.get a deleted SpotImage")
+
+        #JPEG
+        response = c.get(self.jpeg_url)
+        etag = response["ETag"]
+
+        response = c.delete(self.jpeg_url, If_Match=etag)
+
+        self.assertEquals(response.status_code, 410, "Gives a GONE in response to a valid delete")
+
+        response = c.get(self.jpeg_url)
+        self.assertEquals(response.status_code, 404, "Gives a 404 on GET after a delete")
+
+        response = c.delete(self.jpeg_url)
+        self.assertEquals(response.status_code, 404, "Gives a 404 on DELETE after a delete")
+
+        try:
+            test_jpeg= SpotImage.objects.get(pk=self.jpeg.pk)
+        except Exception as e:
+            test_jpeg= None
+
+        self.assertIsNone(test_jpeg, "Can't objects.get a deleted SpotImage")
+
+        #PNG
+        response = c.get(self.png_url)
+        etag = response["ETag"]
+
+        response = c.delete(self.png_url, If_Match=etag)
+
+        self.assertEquals(response.status_code, 410, "Gives a GONE in response to a valid delete")
+
+        response = c.get(self.png_url)
+        self.assertEquals(response.status_code, 404, "Gives a 404 on GET after a delete")
+
+        response = c.delete(self.png_url)
+        self.assertEquals(response.status_code, 404, "Gives a 404 on DELETE after a delete")
+
+        try:
+            test_png= SpotImage.objects.get(pk=self.png.pk)
+        except Exception as e:
+            test_png= None
+
+        self.assertIsNone(test_png, "Can't objects.get a deleted SpotImage")
 

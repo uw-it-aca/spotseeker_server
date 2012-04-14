96eb1e170d80b777d208fdf1f32abd7424591316
Craig M. Stimmel <cstimmel@uw.edu>
2012-04-14 08:43:21 -0700

    Updated delete test setUp to use TEST_ROOT and create SpotImages via spot.spotimage_set.create()

--- spotseeker_server/test/images/delete.py
+++ spotseeker_server/test/images/delete.py
@@ -1,24 +1,37 @@
 from django.utils import unittest
 from django.test.client import Client
-from spotseeker_server.models import Spot
+from django.core.files import File
+from spotseeker_server.models import Spot, SpotImage
+from os.path import abspath, dirname
+
+TEST_ROOT = abspath(dirname(__file__))
 
 class SpotImageDELETETest(unittest.TestCase):
     def setUp(self):
-        spot = Spot.objects.create( name = "This is to test deleting images" )
+        spot = Spot.objects.create( name = "This is to test getting images" )
         spot.save()
         self.spot = spot
 
-        self.url = '/api/v1/spot/{0}'.format(self.spot.pk)
-
-        c = Client()
-        f = open('test/resources/test_jpeg.jpg')
-        response = c.post(self.url, { "description": "This is a jpeg", "image": f })
+        f = open("%s/../resources/test_gif.gif" % TEST_ROOT)
+        gif = self.spot.spotimage_set.create( description = "This is the GIF test", image = File(f) )
+        gif.save()
         f.close()
 
-        f = open('test/resources/test_gif.gif')
-        response = c.post(self.url, { "description": "This is a gif", "image": f })
+        self.gif = gif
+
+        f = open("%s/../resources/test_jpeg.jpg" % TEST_ROOT)
+        jpeg = self.spot.spotimage_set.create( description = "This is the JPEG test", image = File(f) )
+        jpeg.save()
         f.close()
 
-        f = open('test/resources/test_png.png')
-        response = c.post(self.url, { "description": "This is a png", "image": f })
+        self.jpeg = jpeg
+
+        f = open("%s/../resources/test_png.png" % TEST_ROOT)
+        png = self.spot.spotimage_set.create( description = "This is the PNG test", image = File(f) )
+        png.save()
         f.close()
+
+        self.png = png
+
+        self.url = '/api/v1/spot/{0}/image'.format(self.spot.pk)
+        self.url = self.url

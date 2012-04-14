0663a3d55d47582e96f1876cdeab3df0c6ce4f20
Craig M. Stimmel <cstimmel@uw.edu>
2012-04-14 08:34:56 -0700

    TEST_ROOT ensures we can find the test images.

--- spotseeker_server/test/images/get.py
+++ spotseeker_server/test/images/get.py
@@ -4,7 +4,10 @@ from django.core.files import File
 from spotseeker_server.models import Spot, SpotImage
 from cStringIO import StringIO
 from PIL import Image
+from os.path import abspath, dirname
 import random
+
+TEST_ROOT = abspath(dirname(__file__))
 
 class SpotImageGETTest(unittest.TestCase):
     def setUp(self):
@@ -12,21 +15,21 @@ class SpotImageGETTest(unittest.TestCase):
         spot.save()
         self.spot = spot
 
-        f = open('test/resources/test_gif.gif')
+        f = open("%s/../resources/test_gif.gif" % TEST_ROOT)
         gif = SpotImage.objects.create( description = "This is the GIF test", spot=spot, image = File(f) )
         gif.save()
         f.close()
 
         self.gif = gif
 
-        f = open('test/resources/test_jpeg.jpg')
+        f = open("%s/../resources/test_jpeg.jpg" % TEST_ROOT)
         jpeg = SpotImage.objects.create( description = "This is the JPEG test", spot=spot, image = File(f) )
         jpeg.save()
         f.close()
 
         self.jpeg = jpeg
 
-        f = open('test/resources/test_png.png')
+        f = open("%s/../resources/test_png.png" % TEST_ROOT)
         png = SpotImage.objects.create( description = "This is the PNG test", spot=spot, image = File(f) )
         png.save()
         f.close()
@@ -43,7 +46,7 @@ class SpotImageGETTest(unittest.TestCase):
         im = Image.open(data)
         self.assertEquals(response["Content-type"], "image/jpeg", "Content type is jpeg")
 
-        orig = Image.open('test/resources/test_jpeg.jpg')
+        orig = Image.open("%s/../resources/test_jpeg.jpg" % TEST_ROOT)
 
         self.assertEquals(im.size[0], orig.size[0], "Width matches original")
         self.assertEquals(im.size[1], orig.size[1], "Height matches original")
@@ -56,7 +59,7 @@ class SpotImageGETTest(unittest.TestCase):
         im = Image.open(data)
         self.assertEquals(response["Content-type"], "image/gif", "Content type is gif")
 
-        orig = Image.open('test/resources/test_gif.gif')
+        orig = Image.open("%s/../resources/test_gif.gif" % TEST_ROOT)
 
         self.assertEquals(im.size[0], orig.size[0], "Width matches original")
         self.assertEquals(im.size[1], orig.size[1], "Height matches original")
@@ -69,7 +72,7 @@ class SpotImageGETTest(unittest.TestCase):
         im = Image.open(data)
         self.assertEquals(response["Content-type"], "image/png", "Content type is png")
 
-        orig = Image.open('test/resources/test_png.png')
+        orig = Image.open("%s/../resources/test_png.png" % TEST_ROOT)
 
         self.assertEquals(im.size[0], orig.size[0], "Width matches original")
         self.assertEquals(im.size[1], orig.size[1], "Height matches original")

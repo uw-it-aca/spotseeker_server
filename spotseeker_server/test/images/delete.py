1ad4768575f3d8a4e32d2ee9987c4c8d568e1f77
Craig M. Stimmel <cstimmel@uw.edu>
2012-04-14 08:53:38 -0700

    Added test for delete bad url.

--- spotseeker_server/test/images/delete.py
+++ spotseeker_server/test/images/delete.py
@@ -12,26 +12,43 @@ class SpotImageDELETETest(unittest.TestCase):
         spot.save()
         self.spot = spot
 
+        self.url = '/api/v1/spot/{0}'.format(self.spot.pk)
+        self.url = self.url
+
+        # GIF
         f = open("%s/../resources/test_gif.gif" % TEST_ROOT)
         gif = self.spot.spotimage_set.create( description = "This is the GIF test", image = File(f) )
         gif.save()
         f.close()
 
         self.gif = gif
+        self.gif_url = "%s/image/%s" % (self.url, self.gif.pk)
 
+        # JPEG
         f = open("%s/../resources/test_jpeg.jpg" % TEST_ROOT)
         jpeg = self.spot.spotimage_set.create( description = "This is the JPEG test", image = File(f) )
         jpeg.save()
         f.close()
 
         self.jpeg = jpeg
+        self.jpeg_url = "%s/image/%s" % (self.url, self.jpeg.pk)
 
+        # PNG
         f = open("%s/../resources/test_png.png" % TEST_ROOT)
         png = self.spot.spotimage_set.create( description = "This is the PNG test", image = File(f) )
         png.save()
         f.close()
 
         self.png = png
+        self.png_url = "%s/image/%s" % (self.url, self.png.pk)
 
-        self.url = '/api/v1/spot/{0}/image'.format(self.spot.pk)
-        self.url = self.url
+    def test_bad_url(self):
+        c = Client()
+        response = c.delete(self.gif_url)
+        self.assertEquals(response.status_code, 404, "Rejects an invalid url")
+
+        response = c.delete(self.jpeg_url)
+        self.assertEquals(response.status_code, 404, "Rejects an invalid url")
+        
+        response = c.delete(self.png_url)
+        self.assertEquals(response.status_code, 404, "Rejects an invalid url")

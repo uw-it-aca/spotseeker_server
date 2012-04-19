93338aff11a66b89f8ded0823030c63ff22b63ab
Patrick Michaud <pmichaud@uw.edu>
2012-04-19 16:53:27 -0700

    Makes the test line up w/ currently required fields

--- spotseeker_server/test/spot_form.py
+++ spotseeker_server/test/spot_form.py
@@ -5,6 +5,8 @@ from django.conf import settings
 
 class SpotFormTest(unittest.TestCase):
     settings.SPOTSEEKER_AUTH_MODULE = 'spotseeker_server.auth.all_ok';
+    settings.SPOTSEEKER_SPOT_FORM = 'spotseeker_server.default_forms.spot.DefaultSpotForm';
+
     def test_default(self):
         form = SpotForm({})
         self.assertEqual(form.__class__, DefaultSpotForm({}).__class__, "Tests shouldn't be run with a defined SPOTSEEKER_SPOT_FORM")
@@ -13,5 +15,5 @@ class SpotFormTest(unittest.TestCase):
         form = SpotForm({})
         errors = form.errors
         self.assertTrue("name" in errors, "Default spot form requires a spot name")
-        self.assertTrue("capacity" in errors, "Default spot form requires a spot name")
+        self.assertFalse("capacity" in errors, "Default spot form doesn't require a spot name")
 

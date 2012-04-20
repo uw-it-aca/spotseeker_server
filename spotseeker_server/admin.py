82da325b2f1e30400f75cbf935767fa8adb584c3
Pauline Wu <paulinew@almondjoy.cac.washington.edu>
2012-04-20 14:55:23 -0700

    Added a couple of blank lines, format suggested by pep8.

--- spotseeker_server/admin.py
+++ spotseeker_server/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from spotseeker_server.models import *
 
+
 class SpotAdmin(admin.ModelAdmin):
     exclude = ('etag',)
 admin.site.register(Spot, SpotAdmin)
 
 admin.site.register(SpotAvailableHours)
 admin.site.register(SpotExtendedInfo)
+
 
 class SpotImageAdmin(admin.ModelAdmin):
     exclude = ('content_type', 'width', 'height', 'etag',)

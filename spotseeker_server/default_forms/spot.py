97c86953d8282d79e41b145a0a734f6ea5fff53a
Pauline Wu <paulinew@uw.edu>
2012-04-23 11:10:15 -0700

    Removed blank line. Added a line. Pep8 polish

--- spotseeker_server/default_forms/spot.py
+++ spotseeker_server/default_forms/spot.py
@@ -1,10 +1,10 @@
 from django import forms
 from spotseeker_server.models import Spot
 
+
 class DefaultSpotForm(forms.ModelForm):
     name = forms.CharField(max_length=100)
 
     class Meta:
         model = Spot
         fields = ('name', 'capacity')
-

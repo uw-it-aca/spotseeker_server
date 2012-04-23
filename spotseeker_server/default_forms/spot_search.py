538f3b25cef334b78a6cfbd0b7b64dbdc98e602b
Pauline Wu <paulinew@uw.edu>
2012-04-23 11:14:34 -0700

    Added a line, removed a line. Pep8 polish

--- spotseeker_server/default_forms/spot_search.py
+++ spotseeker_server/default_forms/spot_search.py
@@ -1,7 +1,7 @@
 from django import forms
 
+
 class DefaultSpotSearchForm(forms.Form):
     distance = forms.FloatField(required=False)
     center_latitude = forms.FloatField(required=False, max_value=90, min_value=-90)
     center_longitude = forms.FloatField(required=False, max_value=180, min_value=-180)
-

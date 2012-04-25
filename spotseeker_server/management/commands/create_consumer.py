ed4ecfad47e34706301055309be8ff3bc78ae0e9
Craig M. Stimmel <cstimmel@uw.edu>
2012-04-25 12:26:46 -0700

    Optionally take a consumer name as an argument, and self.stdout is the recommended way to return the output of the command.

--- spotseeker_server/management/commands/create_consumer.py
+++ spotseeker_server/management/commands/create_consumer.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
 import hashlib
 import time
 import random
@@ -7,13 +8,23 @@ from oauth_provider.models import Consumer
 class Command(BaseCommand):
     help = 'Creates a unique key and secret for clients connecting to the server'
 
+    option_list = BaseCommand.option_list + (
+        make_option('--name',
+            dest='name',
+            default=False,
+            help='A name for the consumer'),
+        )
+
     def handle(self, *args, **options):
-        consumer_name = raw_input('Enter consumer name: ')
+        if options['name']:
+            consumer_name = options['name']
+        else:
+            consumer_name = raw_input('Enter consumer name: ')
 
         key = hashlib.sha1("{0} - {1}".format(random.random(), time.time())).hexdigest()
         secret = hashlib.sha1("{0} - {1}".format(random.random(), time.time())).hexdigest()
 
         consumer = Consumer.objects.create(name=consumer_name, key=key, secret=secret)
 
-        print "Key: ", key
-        print "Secret: ", secret
+        self.stdout.write("Key: %s\n" % key)
+        self.stdout.write("Secret: %s\n" % secret)

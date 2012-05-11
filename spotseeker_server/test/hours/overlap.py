21950458bf3f809a40fa61b9a9cdd36af6df1d7a
Craig M. Stimmel <cstimmel@uw.edu>
2012-05-10 17:26:56 -0700

    Added test for two available hours windows that don't overlap at all.

--- spotseeker_server/test/hours/overlap.py
+++ spotseeker_server/test/hours/overlap.py
@@ -71,7 +71,22 @@ class SpotHoursOverlapTest(unittest.TestCase):
         self.assertEquals(hours1.end_time, new_hours.end_time, "End time is the same")
 
     def test_no_overlap(self):
-        pass
+        """ Tests adding another available hours object that should not get merged into an existing one.
+        """
+        hours1 = SpotAvailableHours.objects.create(spot=self.spot, day="m", start_time="08:00", end_time="12:00")
+        hours2 = SpotAvailableHours.objects.create(spot=self.spot, day="m", start_time="14:00", end_time="18:00")
+
+        # creating hours2 should get those times merged into hours1
+        hours_obj_count = self.spot.spotavailablehours_set.values_list().count()
+        self.assertEquals(hours_obj_count, 2, "Two SpotAvailableHours objects")
+        
+        # check to see that start and end times are correct
+        new_hours = self.spot.spotavailablehours_set.get(pk=1)
+        new_hours2 = self.spot.spotavailablehours_set.get(pk=2)
+        self.assertEquals(hours1.start_time, new_hours.start_time, "Start time is the same")
+        self.assertEquals(hours1.end_time, new_hours.end_time, "End time is the same")
+        self.assertEquals(hours2.start_time, new_hours2.start_time, "Start time is the same")
+        self.assertEquals(hours2.end_time, new_hours2.end_time, "End time is the same")
 
     def test_exact_same_start(self):
         pass

db7842b151227de1cf0232dac9ed5db65da90877
Craig M. Stimmel <cstimmel@uw.edu>
2012-05-10 17:36:02 -0700

    Added test for trying to add the exact same available hours twice.

--- spotseeker_server/test/hours/overlap.py
+++ spotseeker_server/test/hours/overlap.py
@@ -119,4 +119,16 @@ class SpotHoursOverlapTest(unittest.TestCase):
         self.assertEquals(hours1.end_time, new_hours.end_time, "End time is the same")
 
     def test_exact_same_hours(self):
-        pass
+        """ Tests adding the escact same available hours for a spot.
+        """
+        hours1 = SpotAvailableHours.objects.create(spot=self.spot, day="m", start_time="09:00", end_time="14:00")
+        hours2 = SpotAvailableHours.objects.create(spot=self.spot, day="m", start_time="08:00", end_time="14:00")
+
+        # creating hours2 should get those times merged into hours1
+        hours_obj_count = self.spot.spotavailablehours_set.values_list().count()
+        self.assertEquals(hours_obj_count, 1, "Only one SpotAvailableHours object")
+
+        # check to see that start and end times are correct
+        new_hours = self.spot.spotavailablehours_set.get(pk=1)
+        self.assertEquals(hours1.start_time, new_hours.start_time, "Start time is the same")
+        self.assertEquals(hours1.end_time, new_hours.end_time, "End time is the same")

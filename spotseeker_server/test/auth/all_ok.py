8913ef7deb42024ab0d3464eb78d45f1cb307b8a
Craig M. Stimmel <cstimmel@uw.edu>
2012-06-04 10:35:36 -0700

    Moved all_ok auth tests to explicitly use the auth modules and spot forms just in the scope of the tests regardless of what is set in the settings.py.

--- spotseeker_server/test/auth/all_ok.py
+++ spotseeker_server/test/auth/all_ok.py
@@ -1,14 +1,13 @@
-from django.utils import unittest
+from django.test import TestCase
 from django.conf import settings
 from django.test.client import Client
 from spotseeker_server.models import Spot
 import simplejson as json
 
 
-class SpotAuthAllOK(unittest.TestCase):
+class SpotAuthAllOK(TestCase):
     """ Tests that the all_ok auth module successfully allows any client access.
     """
-    settings.SPOTSEEKER_AUTH_MODULE = 'spotseeker_server.auth.all_ok';
 
     def setUp(self):
         spot = Spot.objects.create(name="This is for testing the all ok auth module", capacity=10 )
@@ -16,23 +15,26 @@ class SpotAuthAllOK(unittest.TestCase):
         self.url = "/api/v1/spot/%s" % self.spot.pk
 
     def test_get(self):
-        c = Client()
-        response = c.get(self.url)
-        spot_dict = json.loads(response.content)
-        returned_spot = Spot.objects.get(pk=spot_dict['id'])
-        self.assertEquals(returned_spot, self.spot, "Returns the correct spot")
+        with self.settings(SPOTSEEKER_AUTH_MODULE='spotseeker_server.auth.all_ok'):
+            c = Client()
+            response = c.get(self.url)
+            spot_dict = json.loads(response.content)
+            returned_spot = Spot.objects.get(pk=spot_dict['id'])
+            self.assertEquals(returned_spot, self.spot, "Returns the correct spot")
 
     def test_put(self):
-        c = Client()
+        with self.settings(SPOTSEEKER_AUTH_MODULE='spotseeker_server.auth.all_ok',
+                           SPOTSEEKER_SPOT_FORM='spotseeker_server.default_forms.spot.DefaultSpotForm'):
+            c = Client()
 
-        response = c.get(self.url)
-        etag = response["ETag"]
+            response = c.get(self.url)
+            etag = response["ETag"]
 
-        spot_dict = json.loads(response.content)
-        spot_dict['name'] = "Modifying all ok"
+            spot_dict = json.loads(response.content)
+            spot_dict['name'] = "Modifying all ok"
 
-        response = c.put(self.url, json.dumps(spot_dict), content_type="application/json", If_Match=etag)
-        self.assertEquals(response.status_code, 200, "Accepts a valid json string")
+            response = c.put(self.url, json.dumps(spot_dict), content_type="application/json", If_Match=etag)
+            self.assertEquals(response.status_code, 200, "Accepts a valid json string")
 
-        updated_spot = Spot.objects.get(pk=self.spot.pk)
-        self.assertEquals(updated_spot.name, "Modifying all ok", "a valid PUT changes the name")
+            updated_spot = Spot.objects.get(pk=self.spot.pk)
+            self.assertEquals(updated_spot.name, "Modifying all ok", "a valid PUT changes the name")

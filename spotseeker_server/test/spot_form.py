513b9ed2cc9756ebbff85f74d4a437125386586b
Craig M. Stimmel <cstimmel@uw.edu>
2012-06-04 09:39:34 -0700

    Moved spot form tests to explicitly use the all_ok auth module and DefaultSpotForm just in the scope of the tests regardless of what is set in the settings.py.

--- spotseeker_server/test/spot_form.py
+++ spotseeker_server/test/spot_form.py
@@ -1,19 +1,21 @@
-from django.utils import unittest
+from django.test import TestCase
 from spotseeker_server.default_forms.spot import DefaultSpotForm
 from spotseeker_server.forms.spot import SpotForm
 from django.conf import settings
 
 
-class SpotFormTest(unittest.TestCase):
-    settings.SPOTSEEKER_AUTH_MODULE = 'spotseeker_server.auth.all_ok'
-    settings.SPOTSEEKER_SPOT_FORM = 'spotseeker_server.default_forms.spot.DefaultSpotForm'
+class SpotFormTest(TestCase):
 
     def test_default(self):
-        form = SpotForm({})
-        self.assertEqual(form.__class__, DefaultSpotForm({}).__class__, "Tests shouldn't be run with a defined SPOTSEEKER_SPOT_FORM")
+        with self.settings(SPOTSEEKER_AUTH_MODULE='spotseeker_server.auth.all_ok',
+                           SPOTSEEKER_SPOT_FORM='spotseeker_server.default_forms.spot.DefaultSpotForm'):
+            form = SpotForm({})
+            self.assertEqual(form.__class__, DefaultSpotForm({}).__class__, "Tests shouldn't be run with a defined SPOTSEEKER_SPOT_FORM")
 
     def test_errors(self):
-        form = SpotForm({})
-        errors = form.errors
-        self.assertTrue("name" in errors, "Default spot form requires a spot name")
-        self.assertFalse("capacity" in errors, "Default spot form doesn't require a spot name")
+        with self.settings(SPOTSEEKER_AUTH_MODULE='spotseeker_server.auth.all_ok',
+                           SPOTSEEKER_SPOT_FORM='spotseeker_server.default_forms.spot.DefaultSpotForm'):
+            form = SpotForm({})
+            errors = form.errors
+            self.assertTrue("name" in errors, "Default spot form requires a spot name")
+            self.assertFalse("capacity" in errors, "Default spot form doesn't require a spot name")

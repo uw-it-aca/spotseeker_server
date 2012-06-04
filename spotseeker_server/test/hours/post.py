57cbcffecd15637f585f5c78862e3a34a4b5632e
Craig M. Stimmel <cstimmel@uw.edu>
2012-06-04 10:53:07 -0700

    Moved hours PUT and POST tests to explicitly use the auth modules and spot forms just in the scope of the tests regardless of what is set in the settings.py.

--- spotseeker_server/test/hours/post.py
+++ spotseeker_server/test/hours/post.py
@@ -1,39 +1,40 @@
-from django.utils import unittest
+from django.test import TestCase
 from django.conf import settings
 from django.test.client import Client
 from spotseeker_server.models import Spot, SpotAvailableHours
 import simplejson as json
 
-class SpotHoursPOSTTest(unittest.TestCase):
-    settings.SPOTSEEKER_AUTH_MODULE = 'spotseeker_server.auth.all_ok';
-    settings.SPOTSEEKER_SPOT_FORM = 'spotseeker_server.default_forms.spot.DefaultSpotForm';
+
+class SpotHoursPOSTTest(TestCase):
 
     def test_hours(self):
+        with self.settings(SPOTSEEKER_AUTH_MODULE='spotseeker_server.auth.all_ok',
+                           SPOTSEEKER_SPOT_FORM='spotseeker_server.default_forms.spot.DefaultSpotForm'):
 
-        post_obj = {
-            'name': "This spot has available hours",
-            'capacity': 4,
-            'available_hours': {
-                'monday': [["00:00", "10:00"], ["11:00", "14:00"]],
-                'tuesday': [["11:00", "14:00"]],
-                'wednesday': [["11:00", "14:00"]],
-                'thursday': [["11:00", "14:00"]],
-                'friday': [["11:00", "14:00"]],
-                'saturday': [],
-                'sunday': [["11:00", "14:00"]],
+            post_obj = {
+                'name': "This spot has available hours",
+                'capacity': 4,
+                'available_hours': {
+                    'monday': [["00:00", "10:00"], ["11:00", "14:00"]],
+                    'tuesday': [["11:00", "14:00"]],
+                    'wednesday': [["11:00", "14:00"]],
+                    'thursday': [["11:00", "14:00"]],
+                    'friday': [["11:00", "14:00"]],
+                    'saturday': [],
+                    'sunday': [["11:00", "14:00"]],
+                }
             }
-        }
 
-        c = Client()
-        response = c.post("/api/v1/spot/", json.dumps(post_obj) , content_type="application/json")
+            c = Client()
+            response = c.post("/api/v1/spot/", json.dumps(post_obj), content_type="application/json")
 
-        get_response = c.get(response["Location"])
+            get_response = c.get(response["Location"])
 
-        self.assertEquals(get_response.status_code, 200, "OK in response to GETing the new spot")
+            self.assertEquals(get_response.status_code, 200, "OK in response to GETing the new spot")
 
-        spot_dict = json.loads(get_response.content)
+            spot_dict = json.loads(get_response.content)
 
-        self.maxDiff = None
-        self.assertEquals(spot_dict["available_hours"], post_obj["available_hours"], "Data from the web service matches the data for the spot")
-        self.assertEquals(spot_dict["name"], post_obj["name"], "Data from the web service matches the data for the spot")
-        self.assertEquals(spot_dict["capacity"], post_obj["capacity"], "Data from the web service matches the data for the spot")
+            self.maxDiff = None
+            self.assertEquals(spot_dict["available_hours"], post_obj["available_hours"], "Data from the web service matches the data for the spot")
+            self.assertEquals(spot_dict["name"], post_obj["name"], "Data from the web service matches the data for the spot")
+            self.assertEquals(spot_dict["capacity"], post_obj["capacity"], "Data from the web service matches the data for the spot")
--- spotseeker_server/test/hours/put.py
+++ spotseeker_server/test/hours/put.py
@@ -1,34 +1,36 @@
-from django.utils import unittest
+from django.test import TestCase
 from django.conf import settings
 from django.test.client import Client
 from spotseeker_server.models import Spot, SpotAvailableHours
 import simplejson as json
 
-class SpotHoursPUTTest(unittest.TestCase):
-    settings.SPOTSEEKER_AUTH_MODULE = 'spotseeker_server.auth.all_ok';
+
+class SpotHoursPUTTest(TestCase):
 
     def test_hours(self):
-        spot = Spot.objects.create(name = "This spot has available hours" )
-        etag = spot.etag
+        with self.settings(SPOTSEEKER_AUTH_MODULE='spotseeker_server.auth.all_ok',
+                       SPOTSEEKER_SPOT_FORM='spotseeker_server.default_forms.spot.DefaultSpotForm'):
+            spot = Spot.objects.create(name="This spot has available hours")
+            etag = spot.etag
 
-        put_obj = {
-            'name': "This spot has available hours",
-            'capacity': "4",
-            'available_hours': {
-                'monday': [["00:00", "10:00"], ["11:00", "14:00"]],
-                'tuesday': [["11:00", "14:00"]],
-                'wednesday': [["11:00", "14:00"]],
-                'thursday': [["11:00", "14:00"]],
-                'friday': [["11:00", "14:00"]],
-                'saturday': [],
-                'sunday': [["11:00", "14:00"]],
+            put_obj = {
+                'name': "This spot has available hours",
+                'capacity': "4",
+                'available_hours': {
+                    'monday': [["00:00", "10:00"], ["11:00", "14:00"]],
+                    'tuesday': [["11:00", "14:00"]],
+                    'wednesday': [["11:00", "14:00"]],
+                    'thursday': [["11:00", "14:00"]],
+                    'friday': [["11:00", "14:00"]],
+                    'saturday': [],
+                    'sunday': [["11:00", "14:00"]],
+                }
             }
-        }
 
-        c = Client()
-        url = "/api/v1/spot/%s" % spot.pk
-        response = c.put(url, json.dumps(put_obj) , content_type="application/json", If_Match=etag )
-        spot_dict = json.loads(response.content)
+            c = Client()
+            url = "/api/v1/spot/%s" % spot.pk
+            response = c.put(url, json.dumps(put_obj), content_type="application/json", If_Match=etag)
+            spot_dict = json.loads(response.content)
 
-        self.maxDiff = None
-        self.assertEquals(spot_dict["available_hours"], put_obj["available_hours"], "Data from the web service matches the data for the spot")
+            self.maxDiff = None
+            self.assertEquals(spot_dict["available_hours"], put_obj["available_hours"], "Data from the web service matches the data for the spot")

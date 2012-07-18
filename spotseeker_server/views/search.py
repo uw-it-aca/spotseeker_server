850053515009a7e99e4386f7104319ee17fe4281
Patrick Michaud <pmichaud@uw.edu>
2012-07-18 16:21:50 -0700

    Adds excludes based on the noise_level provided - spot-186

--- spotseeker_server/views/search.py
+++ spotseeker_server/views/search.py
@@ -94,6 +94,37 @@ class SearchView(RESTDispatch):
                         day = day_dict[day]
                         query = query.filter(spotavailablehours__day__iexact=day, spotavailablehours__start_time__lte=time, spotavailablehours__end_time__gt=time)
                         has_valid_search_param = True
+            elif key == "extended_info:noise_level":
+                noise_levels = request.GET.getlist("extended_info:noise_level")
+
+                exclude_silent = True
+                exclude_quiet = True
+                exclude_moderate = True
+                exclude_loud = True
+                exclude_variable = True
+
+                for level in noise_levels:
+                    if "silent" == level:
+                        exclude_silent = False
+                    if "quiet" == level:
+                        exclude_quiet = False
+                        exclude_variable = False
+                    if "moderate" == level:
+                        exclude_quiet = False
+                        exclude_variable = False
+
+                if exclude_silent:
+                    query = query.exclude(spotextendedinfo__key="noise_level", spotextendedinfo__value__iexact="silent")
+                if exclude_quiet:
+                    query = query.exclude(spotextendedinfo__key="noise_level", spotextendedinfo__value__iexact="quiet")
+                if exclude_moderate:
+                    query = query.exclude(spotextendedinfo__key="noise_level", spotextendedinfo__value__iexact="moderate")
+                if exclude_loud:
+                    query = query.exclude(spotextendedinfo__key="noise_level", spotextendedinfo__value__iexact="loud")
+                if exclude_variable:
+                    query = query.exclude(spotextendedinfo__key="noise_level", spotextendedinfo__value__iexact="variable")
+
+
             elif key == "capacity":
                 try:
                     limit = int(request.GET["capacity"])

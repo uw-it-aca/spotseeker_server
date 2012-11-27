76faf4b67c84da6b0221edfbcab1507d48fba0d9
Craig M. Stimmel <cstimmel@uw.edu>
2012-11-27 11:42:46 -0800

    Built out the building/campus search unit tests a bit more.

--- test/search/buildings.py
+++ test/search/buildings.py
@@ -11,28 +11,37 @@ class BuildingSearchTest(TestCase):
     def test_buildings_for_campus(self):
         spot1 = Spot.objects.create(name="Spot on campus A.", building_name="Aay building")
         spot2 = Spot.objects.create(name="Spot on campus B.", building_name="Bee building")
+        spot3 = Spot.objects.create(name="Another Spot on campus B.", building_name="Bee building")
+        spot4 = Spot.objects.create(name="Yet Another Spot on campus B.", building_name="Cee building")
 
-        campusa = SpotExtendedInfo.objects.create(spot=spot1, key="campus", value="seattle")
-        campusa.save()
-        campusb = SpotExtendedInfo.objects.create(spot=spot2, key="campus", value="tacoma")
-        campusb.save()
+        spot1_campus = SpotExtendedInfo.objects.create(spot=spot1, key="campus", value="campus_a")
+        spot1_campus.save()
+        spot2_campus = SpotExtendedInfo.objects.create(spot=spot2, key="campus", value="campus_b")
+        spot2_campus.save()
+        spot3_campus = SpotExtendedInfo.objects.create(spot=spot3, key="campus", value="campus_b")
+        spot2_campus.save()
+        spot4_campus = SpotExtendedInfo.objects.create(spot=spot4, key="campus", value="campus_b")
+        spot2_campus.save()
 
         c = Client()
 
         response = c.get("/api/v1/buildings")
         buildings = json.loads(response.content)
-        self.assertEquals(len(buildings), 2, 'Full Building list returns 2 building.')
+        self.assertEquals(len(buildings), 3, 'Full Building list returns 3 building.')
 
-        response = c.get("/api/v1/buildings", {"campus": "seattle"})
+        response = c.get("/api/v1/buildings", {"campus": "campus_a"})
         buildings = json.loads(response.content)
 
-        self.assertEquals(len(buildings), 1, 'Seattle building list only returns 1 building.')
+        self.assertEquals(len(buildings), 1, 'Campus A building list only returns 1 building.')
         # Assert that the building returned is not from the tacoma campus.
+        self.assertNotEqual(buildings[0], spot2.building_name, "Returned building is not the one on campus B.")
         # Assert that the building returned is from the seattle campus.
+        self.assertEquals(buildings[0], spot1.building_name, "Returned building is the one on campus A.")
 
-        response = c.get("/api/v1/buildings", {"campus": "tacoma"})
+        response = c.get("/api/v1/buildings", {"campus": "campus_b"})
         buildings = json.loads(response.content)
 
-        self.assertEquals(len(buildings), 1, 'Seattle building list only returns 1 building.')
-        # Assert that the building returned is not from the seattle campus.
-        # Assert that the building returned is from the tacoma campus.
+        self.assertEquals(len(buildings), 2, 'Campus B building list returns 2 buildings.')
+        for building in buildings:
+            # Assert that the building returned is not from the seattle campus.
+            self.assertNotEqual(building, spot1.building_name, "Returned building is not the one on campus A.")

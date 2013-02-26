c3b7fcebe5706cd191461107eeee580ac4c24639
Sam Olds <samolds@uw.edu>
2013-02-26 13:44:05 -0800

    POSTing no longer leaves blank spots if extended info is invalid
    fixes SPOT-653
    
    fixed PUTting with invalid extended info in the last commit, but
    that broke POSTing, so this fixes both. also provided unit tests
    for POSTing in addition to the PUT one in the last commit

--- views/spot.py
+++ views/spot.py
@@ -104,6 +104,8 @@ class SpotView(RESTDispatch):
 
         form = SpotForm(new_values)
         if not form.is_valid():
+            if request.method == 'POST':
+                spot.delete()
             response = HttpResponse(json.dumps(form.errors))
             response.status_code = 400
             return response

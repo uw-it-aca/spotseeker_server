0b3650c53e7e687e476c2d5a6fee68d142ee3510
Sam Olds <samolds@uw.edu>
2013-07-26 11:34:22 -0700

    fixed the UWSpotForm unit tests to work with the new validation provided by uiuc
    fixes SPOT-1023

--- test/uw_spot/spot_form.py
+++ test/uw_spot/spot_form.py
@@ -14,14 +14,38 @@
 """
 
 from django.test import TestCase
-from spotseeker_server.org_forms.uw_spot import UWSpotForm
+from spotseeker_server.org_forms.uw_spot import UWSpotForm, UWSpotExtendedInfoForm
 from django.conf import settings
+from django.test.utils import override_settings
 
 
+@override_settings(SPOTSEEKER_AUTH_MODULE='spotseeker_server.auth.all_ok')
+@override_settings(SPOTSEEKER_SPOT_FORM='spotseeker_server.org_forms.uw_spot.UWSpotForm')
+@override_settings(SPOTSEEKER_SPOTEXTENDEDINFO_FORM='spotseeker_server.org_forms.uw_spot.UWSpotExtendedInfoForm')
 class UWSpotFormTest(TestCase):
+    def test_spot_form_errors(self):
+        form = UWSpotForm({})
+        errors = form.errors
+        self.assertTrue("name" in errors, "UW spot form requires a name")
+        self.assertTrue("latitude" in errors, "UW spot form requires latitude")
+        self.assertTrue("longitude" in errors, "UW spot form requires longitude")
 
-    def test_errors(self):
-        with self.settings(SPOTSEEKER_AUTH_MODULE='spotseeker_server.auth.all_ok'):
-            form = UWSpotForm({})
-            errors = form.errors
-            self.assertTrue("extended_info" in errors, "UW spot form requires extended_info")
+    def test_spotextendedinfo_form_errors(self):
+        form = UWSpotForm({"name": "fo' testing dawg", "latitude": "45", "longitude": "-125"})
+        self.assertEquals({}, form.errors, "UW SpotForm error.")
+        spot = form.save()
+
+        a_form = UWSpotExtendedInfoForm({"spot": spot.id, "key": "noise_level", "value": "quiet"})
+        b_form = UWSpotExtendedInfoForm({"spot": spot.id, "key": "has_computers", "value": "true"})
+        c_form = UWSpotExtendedInfoForm({"spot": spot.id, "key": "orientation", "value": "North"})
+        d_form = UWSpotExtendedInfoForm({"spot": spot.id, "key": "num_computers", "value": 15})
+
+        a_errors = a_form.errors
+        b_errors = b_form.errors
+        c_errors = c_form.errors
+        d_errors = d_form.errors
+
+        self.assertEquals({}, a_errors, "UW SpotExtendedInfo noise_level error.")
+        self.assertEquals({}, b_errors, "UW SpotExtendedInfo has_computers error.")
+        self.assertEquals({}, c_errors, "UW SpotExtendedInfo orientation error.")
+        self.assertEquals({}, d_errors, "UW SpotExtendedInfo num_computers error.")

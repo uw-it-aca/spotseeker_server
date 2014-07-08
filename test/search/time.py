f1f73b2ce83185d4fed633aec5db24632f3f471e
Cole Bessee <cbessee@uw.edu>
2014-07-08 14:44:04 -0700

    Adds patricks proposed tests to DiffDayTimeInSerial method

--- test/search/time.py
+++ test/search/time.py
@@ -15,8 +15,11 @@
 
 from django.test import TestCase
 from django.test.client import Client
+from django.test.utils import override_settings
 from spotseeker_server.models import Spot, SpotAvailableHours
 
+
+@override_settings(SPOTSEEKER_AUTH_MODULE='spotseeker_server.auth.all_ok')
 class SpotSearchTimeTest(TestCase):
 
     def setUp(self):
@@ -53,21 +56,46 @@ class SpotSearchTimeTest(TestCase):
         spot2 = Spot.objects.create(name="This is a second spot to test time ranges in search")
         spot2.save()
 
-        availhours = SpotAvailableHours.objects.create(spot=spot, day="th", start_time="11:00:00", end_time="23:59:59")
-        availhours2 = SpotAvailableHours.objects.create(spot=spot, day="f", start_time="00:00:00", end_time="16:00:00")
-        availhours3 = SpotAvailableHours.objects.create(spot=spot2, day="f", start_time="11:00:00", end_time="16:00:00")
+        spot3 = Spot.objects.create(name="This is a third spot to test time ranges in search")
+        spot3.save()
+
+        availhoursmon = SpotAvailableHours.objects.create(spot=spot, day="m", start_time="00:00:00", end_time="13:00:00")
+        availhoursmon2 = SpotAvailableHours.objects.create(spot=spot, day="m", start_time="14:00:00", end_time="23:59:59")
+        availhourstues = SpotAvailableHours.objects.create(spot=spot, day="t", start_time="00:00:00", end_time="23:59:59")
+        availhoursweds = SpotAvailableHours.objects.create(spot=spot, day="w", start_time="00:00:00", end_time="23:59:59")
+        availhoursthurs = SpotAvailableHours.objects.create(spot=spot, day="th", start_time="00:00:00", end_time="23:59:59")
+        availhoursfri = SpotAvailableHours.objects.create(spot=spot, day="f", start_time="00:00:00", end_time="23:59:59")
+        availhourssat = SpotAvailableHours.objects.create(spot=spot, day="sa", start_time="00:00:00", end_time="23:59:59")
+        availhourssun = SpotAvailableHours.objects.create(spot=spot, day="su", start_time="00:00:00", end_time="23:59:59")
+
+        availhoursmon3 = SpotAvailableHours.objects.create(spot=spot2, day="m", start_time="00:00:00", end_time="13:00:00")
+        availhourstues2 = SpotAvailableHours.objects.create(spot=spot2, day="t", start_time="00:00:00", end_time="23:59:59")
+        availhoursweds2 = SpotAvailableHours.objects.create(spot=spot2, day="w", start_time="00:00:00", end_time="23:59:59")
+        availhoursthurs2 = SpotAvailableHours.objects.create(spot=spot2, day="th", start_time="00:00:00", end_time="23:59:59")
+        availhoursfri2 = SpotAvailableHours.objects.create(spot=spot2, day="f", start_time="00:00:00", end_time="08:00:00")
+        availhoursfri3 = SpotAvailableHours.objects.create(spot=spot2, day="f", start_time="09:00:00", end_time="23:59:59")
+        availhourssat2 = SpotAvailableHours.objects.create(spot=spot2, day="sa", start_time="00:00:00", end_time="23:59:59")
+        availhourssun2 = SpotAvailableHours.objects.create(spot=spot2, day="su", start_time="00:00:00", end_time="23:59:59")
+
+
+        availhoursmon4 = SpotAvailableHours.objects.create(spot=spot3, day="m", start_time="00:00:00", end_time="23:59:59")
+        availhourstues3 = SpotAvailableHours.objects.create(spot=spot3, day="t", start_time="00:00:00", end_time="23:59:59")
+        availhoursweds3 = SpotAvailableHours.objects.create(spot=spot3, day="w", start_time="00:00:00", end_time="23:59:59")
+        availhoursthurs3 = SpotAvailableHours.objects.create(spot=spot3, day="th", start_time="00:00:00", end_time="13:00:00")
+        availhoursthurs4 = SpotAvailableHours.objects.create(spot=spot3, day="th", start_time="14:00:00", end_time="23:59:59")
+        availhoursfri4 = SpotAvailableHours.objects.create(spot=spot3, day="f", start_time="00:00:00", end_time="23:59:59")
+        availhourssat3 = SpotAvailableHours.objects.create(spot=spot3, day="sa", start_time="00:00:00", end_time="23:59:59")
+        availhourssun3 = SpotAvailableHours.objects.create(spot=spot3, day="su", start_time="00:00:00", end_time="23:59:59")
+
 
         c = Client()
 
-        response = c.get('/api/v1/spot/', { 'open_at': "Thursday,11:00", 'open_until': "Friday,14:00"}, content_type='application/json')
+        response = c.get('/api/v1/spot/', { 'open_at': "Monday,10:00", 'open_until': "Friday,10:00"}, content_type='application/json')
 
         self.assertEquals(response.status_code, 200)
-        self.assertContains(response, "This spot is to test time ranges in search")
-
-        response = c.get('/api/v1/spot/', { 'open_at': "Tuesday,11:00", 'open_until': "Wednesday,15:00"}, content_type='application/json')
-
-        self.assertEquals(response.status_code, 200)
-        self.assertContains(response, "[]")
+        self.assertNotContains(response, "This spot is to test time ranges in search")
+        self.assertNotContains(response, "This is a second spot to test time ranges in search")
+        self.assertNotContains(response, "This is a third spot to test time ranges in search")
 
     def SameDayTimeInReverse(self):
         spot = Spot.objects.create(name="This spot is to test time ranges in search")
@@ -93,7 +121,7 @@ class SpotSearchTimeTest(TestCase):
 
         c = Client()
 
-        response = c.get('/api/v1/spot/', { 'open_at': "Monday,13:00", 'open_until': "Monday,11:00"}, content_type='application/json')
+        response = c.get('/api/v1/spot/', { 'open_at': "Monday,15:00", 'open_until': "Monday,10:00"}, content_type='application/json')
 
         self.assertEquals(response.status_code, 200)
         self.assertContains(response, 'This spot is to test time ranges in search')

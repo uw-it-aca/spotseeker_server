9094b9ffaef1123d1a1f995a6281f909d3a3e86a
haughar <haughar@uw.edu>
2015-12-18 10:08:18 -0800

    Add unit tests around changes made to building api.

--- test/search/buildings.py
+++ test/search/buildings.py
@@ -28,6 +28,42 @@ from spotseeker_server import models
 class BuildingSearchTest(TestCase):
     """ Tests the /api/v1/buildings interface.
     """
+
+    def setUp(self):
+        self.spot1 = Spot.objects.create(name="Spot on campus A.", building_name="Aay building")
+        self.spot1_2 = Spot.objects.create(name="Other spot on campus A.", building_name="Aay building")
+        self.spot2 = Spot.objects.create(name="Spot on campus B.", building_name="Bee building")
+        self.spot3 = Spot.objects.create(name="Another Spot on campus B.", building_name="Bee building")
+        self.spot4 = Spot.objects.create(name="Spot on campus C.", building_name="Cee building")
+        self.spot5 = Spot.objects.create(name="Here is a spot on campus D.", building_name='Dee building')
+        self.spot6 = Spot.objects.create(name='Another spot on campus C.', building_name='Cee building')
+        self.spot7 = Spot.objects.create(name='Another spot on campus D.', building_name='Eee building')
+
+        self.spot1_campus = SpotExtendedInfo.objects.create(spot=self.spot1, key="campus", value="campus_a")
+        self.spot1_campus.save()
+        self.spot1_app_type = SpotExtendedInfo.objects.create(spot=self.spot1, key='app_type', value='food')
+        self.spot1_app_type.save()
+        self.spot1_2_campus = SpotExtendedInfo.objects.create(spot=self.spot1_2, key='campus', value='campus_a')
+        self.spot1_2_campus.save()
+        self.spot2_campus = SpotExtendedInfo.objects.create(spot=self.spot2, key="campus", value="campus_b")
+        self.spot2_campus.save()
+        self.spot3_campus = SpotExtendedInfo.objects.create(spot=self.spot3, key="campus", value="campus_b")
+        self.spot3_campus.save()
+        self.spot4_campus = SpotExtendedInfo.objects.create(spot=self.spot4, key="campus", value="campus_c")
+        self.spot4_campus.save()
+        self.spot5_campus = SpotExtendedInfo.objects.create(spot=self.spot5, key='campus', value='campus_d')
+        self.spot5_campus.save()
+        self.spot5_app_type = SpotExtendedInfo.objects.create(spot=self.spot5, key='app_type', value='food')
+        self.spot6_campus = SpotExtendedInfo.objects.create(spot=self.spot6, key='campus', value='campus_c')
+        self.spot6_campus.save()
+        self.spot6_app_type = SpotExtendedInfo.objects.create(spot=self.spot6, key='app_type', value='book')
+        self.spot6_app_type.save()
+        self.spot7_campus = SpotExtendedInfo.objects.create(spot=self.spot7, key='campus', value='campus_d')
+        self.spot7_campus.save()
+        self.spot7_app_type = SpotExtendedInfo.objects.create(spot=self.spot7, key='app_type', value='food')
+        self.spot7_app_type.save()
+
+
     def test_content_type(self):
         dummy_cache = cache.get_cache('django.core.cache.backends.dummy.DummyCache')
         with patch.object(models, 'cache', dummy_cache):
@@ -36,40 +72,75 @@ class BuildingSearchTest(TestCase):
             response = c.get(url)
             self.assertEquals(response["Content-Type"], "application/json", "Has the json header")
 
-    def test_buildings_for_campus(self):
-        spot1 = Spot.objects.create(name="Spot on campus A.", building_name="Aay building")
-        spot2 = Spot.objects.create(name="Spot on campus B.", building_name="Bee building")
-        spot3 = Spot.objects.create(name="Another Spot on campus B.", building_name="Bee building")
-        spot4 = Spot.objects.create(name="Yet Another Spot on campus B.", building_name="Cee building")
-
-        spot1_campus = SpotExtendedInfo.objects.create(spot=spot1, key="campus", value="campus_a")
-        spot1_campus.save()
-        spot2_campus = SpotExtendedInfo.objects.create(spot=spot2, key="campus", value="campus_b")
-        spot2_campus.save()
-        spot3_campus = SpotExtendedInfo.objects.create(spot=spot3, key="campus", value="campus_b")
-        spot2_campus.save()
-        spot4_campus = SpotExtendedInfo.objects.create(spot=spot4, key="campus", value="campus_b")
-        spot2_campus.save()
-
+    def test_get_all_buildings(self):
         c = Client()
-
+        #TODO: Even though this works, we do not recommend using this method. You should be passing at least one query param.
         response = c.get("/api/v1/buildings")
         buildings = json.loads(response.content)
-        self.assertEquals(len(buildings), 3, 'Full Building list returns 3 building.')
+        self.assertEquals(len(buildings), 5)
 
-        response = c.get("/api/v1/buildings", {"campus": "campus_a"})
+    def test_buildings_for_campus(self):
+        c = Client()
+
+        response = c.get("/api/v1/buildings/", {"campus": "campus_a"})
         buildings = json.loads(response.content)
 
-        self.assertEquals(len(buildings), 1, 'Campus A building list only returns 1 building.')
+        self.assertEquals(len(buildings), 1)
         # Assert that the building returned is not from the tacoma campus.
-        self.assertNotEqual(buildings[0], spot2.building_name, "Returned building is not the one on campus B.")
+        self.assertNotEqual(buildings[0], self.spot2.building_name)
         # Assert that the building returned is from the seattle campus.
-        self.assertEquals(buildings[0], spot1.building_name, "Returned building is the one on campus A.")
+        self.assertEquals(buildings[0], self.spot1.building_name)
 
         response = c.get("/api/v1/buildings", {"campus": "campus_b"})
         buildings = json.loads(response.content)
 
-        self.assertEquals(len(buildings), 2, 'Campus B building list returns 2 buildings.')
+        self.assertEquals(len(buildings), 1)
         for building in buildings:
             # Assert that the building returned is not from the seattle campus.
-            self.assertNotEqual(building, spot1.building_name, "Returned building is not the one on campus A.")
+            self.assertNotEqual(building, self.spot1.building_name)
+
+    def test_buildings_for_app_type(self):
+        c = Client()
+
+        response = c.get('/api/v1/buildings/', {'extended_info:app_type':'food'})
+        buildings = json.loads(response.content)
+
+        self.assertEqual(len(buildings), 3)
+        self.assertEqual(buildings[0], self.spot1.building_name)
+        self.assertEqual(buildings[1], self.spot5.building_name)
+
+        response = c.get('/api/v1/buildings/', {'extended_info:app_type':'book'})
+        buildings = json.loads(response.content)
+        self.assertEqual(len(buildings), 1)
+        self.assertEqual(buildings[0], self.spot6.building_name)
+
+    def test_buildings_for_app_type_and_campus(self):
+        c = Client()
+
+        response = c.get('/api/v1/buildings/', {'app_type':'food', 'campus':'campus_a'})
+        buildings = json.loads(response.content)
+        self.assertEqual(len(buildings), 1)
+        self.assertEqual(buildings[0], self.spot1.building_name)
+
+        response = c.get('/api/v1/buildings/', {'app_type':'book', 'campus':'campus_c'})
+        buildings = json.loads(response.content)
+        self.assertEqual(len(buildings), 1)
+        self.assertEqual(buildings[0], self.spot6.building_name)
+
+        response = c.get('/api/v1/buildings/', {'extended_info:app_type':'food', 'campus':'campus_d'})
+        buildings = json.loads(response.content)
+        self.assertEqual(len(buildings), 2)
+        self.assertEqual(buildings[0], self.spot5.building_name)
+        self.assertEqual(buildings[1], self.spot7.building_name)
+
+    def test_extended_info_campus(self):
+        c = Client()
+
+        response = c.get('/api/v1/buildings/', {'extended_info:campus':'campus_c'})
+        buildings = json.loads(response.content)
+        self.assertEqual(len(buildings), 1)
+        self.assertEqual(buildings[0], self.spot6.building_name)
+
+        response = c.get('/api/v1/buildings/', {'extended_info:campus':'campus_d', 'extended_info:app_type':'food'})
+        buildings = json.loads(response.content)
+        self.assertEqual(len(buildings), 2)

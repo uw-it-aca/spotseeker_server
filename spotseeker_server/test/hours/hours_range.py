1c5e3a4bbc4fac38114a5474d7117af68051ec8e
Blair Javidan <armaghan.javidan@gmail.com>
2016-03-10 13:23:16 -0800

    added implementation of test_span_late_night but not passing.

--- spotseeker_server/test/hours/hours_range.py
+++ spotseeker_server/test/hours/hours_range.py
@@ -560,8 +560,34 @@ class HoursRangeTest(TestCase):
         """ Tests a search range where the spot's open time is before the
             start on one day, and the close time is beyond the end of
             range on the next day. Search range: today 20:00 - tomorrow 2:00
-            Not passing with current implementation.
+            Not passing with current implementation so commented out.
         """
+        dummy_cache = cache.get_cache(
+            'django.core.cache.backends.dummy.DummyCache')
+        with patch.object(models, 'cache', dummy_cache):
+            start_query_time = datetime.time(self.now + timedelta(hours=11))
+            start_query_time = start_query_time.strftime("%H:%M")
+            start_query_day = self.day_dict[self.today]
+            start_query = "%s,%s" % (start_query_day, start_query_time)
+
+            end_query_time = datetime.time(self.now - timedelta(hours=7))
+            end_query_time = end_query_time.strftime("%H:%M")
+            end_query_day = self.day_dict[self.tomorrow]
+            end_query = "%s,%s" % (end_query_day, end_query_time)
+
+            client = Client()
+            response = client.get(
+                "/api/v1/spot",
+                {'fuzzy_hours_start': start_query,
+                 'fuzzy_hours_end': end_query})
+            spots = json.loads(response.content)
+
+            #self.assertEqual(response.status_code, 200)
+            #self.assertFalse(self.spot1.json_data_structure() in spots)
+            #self.assertFalse(self.spot2.json_data_structure() in spots)
+            # Don't assert spot3, see docstring above
+            #self.assertTrue(self.spot4.json_data_structure() in spots)
+            # Don't assert spot5, see docstring above
         pass
 
     def tearDown(self):

b99de88054b78738c13410f34a01b12b606e7a29
Ethan Turner <ezturner@washington.edu>
2016-08-17 14:03:11 -0700

    Fixed itemapi error

--- spotseeker_server/forms/item.py
+++ spotseeker_server/forms/item.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
 
-class SpotExtendedInfoForm(object):
+class ItemExtendedInfoForm(object):
     @staticmethod
     def implementation():
         if hasattr(settings, 'SPOTSEEKER_ITEMEXTENDEDINFO_FORM'):
@@ -33,13 +33,13 @@ class SpotExtendedInfoForm(object):
                 raise ImproperlyConfigured('Error importing module %s: "%s"' %
                                            (module, e))
             try:
-                SpotExtendedInfoForm = getattr(mod, attr)
+                ItemExtendedInfoForm = getattr(mod, attr)
             except AttributeError:
                 raise ImproperlyConfigured('Module "%s" does not define '
                                            'a "%s" class.' % (module, attr))
-            return SpotExtendedInfoForm
+            return ItemExtendedInfoForm
         else:
-            return DefaultSpotExtendedInfoForm
+            return DefaultItemExtendedInfoForm
 
     def __new__(*args, **kwargs):
         return ItemExtendedInfoForm.implementation()(args[1], **kwargs)
@@ -61,7 +61,7 @@ class ItemForm(object):
             except AttributeError:
                 raise ImproperlyConfigured('Module "%s" does not define '
                                            'a "%s" class.' % (module, attr))
-            return ItemsForm
+            return ItemForm
         else:
             return DefaultItemForm
 

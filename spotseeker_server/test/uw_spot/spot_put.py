fa61760be3d350914258353a248b5750f48141b2
Ethan Turner <ezturner@washington.edu>
2016-09-02 14:06:52 -0700

    Added tests for phone number cleaning

--- spotseeker_server/test/uw_spot/spot_put.py
+++ spotseeker_server/test/uw_spot/spot_put.py
@@ -22,6 +22,7 @@ import random
 from django.test.utils import override_settings
 from mock import patch
 from spotseeker_server import models
+from spotseeker_server.test import utils_test
 
 
 ALL_OK = 'spotseeker_server.auth.all_ok'
@@ -49,6 +50,30 @@ class UWSpotPUTTest(TransactionTestCase):
 
         url = '/api/v1/spot/{0}'.format(self.spot.pk)
         self.url = url
+
+    def get_etag(self, url):
+        """Returns the ETag from the given URL."""
+        response = self.client.get(url)
+
+        if 'ETag' not in response:
+            print "ETag not found for URL " + url
+
+        return response['ETag']
+
+    def put_spot(self, url, body):
+        """PUTs a spot with the given URL and body.
+
+        Will first fire a GET at that URL, and then retrieve the current ETag
+        for said spot so that we can ensure that we do not cause conflicts.
+
+        Body can either be a string or a dict.
+        """
+        if not isinstance(body, basestring):
+            body = json.dumps(body)
+
+        eTag = self.get_etag(url)
+        return self.client.put(url, body, content_type="application/json",
+                               If_Match=eTag)
 
     def test_bad_json(self):
         c = Client()
@@ -219,3 +244,45 @@ class UWSpotPUTTest(TransactionTestCase):
             self.assertEquals(json.loads(json_string)['extended_info'],
                               json.loads(response.content)['extended_info'],
                               "Doesn't update spot with invalid int value")
+
+    def test_phone_number_validation(self):
+        phone_numbers = (
+            '(425) 274-2853',
+            '(206) 285-2884',
+            '294 203 3829',
+            '273.211.2495',
+            '(293) 284-2848',
+            '(204)-203 2848',
+        )
+
+        formatted_phone_numbers = (
+            '4252742853',
+            '2062852884',
+            '2942033829',
+            '2732112495',
+            '2932842848',
+            '2042032848',
+        )
+
+        spot_json = utils_test.get_spot("Test name", 20)
+
+        spot_json['extended_info']['test_ei'] = 'ei'
+
+        response = self.put_spot(self.url, spot_json)
+
+        self.assertEqual(response.status_code, 200)
+
+        spot_json = json.loads(response.content)
+
+        for number, formatted_number in zip(phone_numbers,
+                                            formatted_phone_numbers):
+            spot_json['extended_info']['s_phone'] = number
+
+            response = self.put_spot(self.url, spot_json)
+
+            self.assertEqual(response.status_code, 200)
+
+            spot_json = json.loads(response.content)
+
+            self.assertEqual(spot_json['extended_info']['s_phone'],
+                             formatted_number)

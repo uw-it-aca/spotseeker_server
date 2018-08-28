3d783cced7bb65a4c699aa4340f60681b3f5d06f
Craig M. Stimmel <cstimmel@uw.edu>
2018-08-28 15:08:37 -0700

    Separate out tests specific to the UW Filter.

--- spotseeker_server/tests.py
+++ spotseeker_server/tests.py
@@ -48,6 +48,7 @@ from spotseeker_server.test.search.distance_fields import \
 from spotseeker_server.test.search.view_methods import \
     SpotSearchViewMethodsTest
 from spotseeker_server.test.search.noise_level import NoiseLevelTestCase
+from spotseeker_server.test.search.uw_noise_level import UWNoiseLevelTestCase
 from spotseeker_server.test.search.time import SpotSearchTimeTest
 from spotseeker_server.test.hours.model import SpotHoursModelTest
 from spotseeker_server.test.hours.get import SpotHoursGETTest

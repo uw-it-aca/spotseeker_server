7a5419233a3003578495fe5ec7000ee83cec7ea7
Craig M. Stimmel <cstimmel@uw.edu>
2018-11-13 14:44:44 -0800

    Scout no longer needs South.

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(name='SpaceScout-Server',
                         'Pillow',
                         'pyproj',
                         'pytz',
-                        'South',
                         'simplejson>=2.1',
                         'django-oauth-plus<=2.2.5',
                         'phonenumbers'

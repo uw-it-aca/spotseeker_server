69133894cbe35bc11c9f2018cd510e00abd288e5
Sarthak Singh <ss269@uw.edu>
2020-06-29 14:29:29 -0700

    Changed get_cache dummy patch to overwrite settings

--- spotseeker_server/test/item/image_get.py
+++ spotseeker_server/test/item/image_get.py
@@ -30,6 +30,12 @@ TEST_ROOT = abspath(dirname(__file__))
 @override_settings(SPOTSEEKER_AUTH_MODULE='spotseeker_server.auth.all_ok')
 class ItemImageGETTest(TestCase):
 
+    dummy_cache_setting = {
+        'default': { 
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        }
+    }
+
     def setUp(self):
         item = Item.objects.create(name="This is to test getting images")
         item.save()
@@ -63,61 +69,54 @@ class ItemImageGETTest(TestCase):
         self.url = '/api/v1/item/{0}/image'.format(self.item.pk)
         self.url = self.url
 
+    @override_settings(CACHES=dummy_cache_setting)
     def test_bad_url(self):
-        dummy_cache = cache.get_cache(
-            'django.core.cache.backends.dummy.DummyCache')
-        with patch.object(models, 'cache', dummy_cache):
-            c = Client()
-            item = Item.objects.create(name="This is the wrong item")
+        c = Client()
+        item = Item.objects.create(name="This is the wrong item")
 
-            response = c.get("/api/v1/item/{0}/image/{1}".
-                             format(item.pk, self.jpeg.pk))
-            self.assertEquals(response.status_code, 404)
+        response = c.get("/api/v1/item/{0}/image/{1}".
+                            format(item.pk, self.jpeg.pk))
+        self.assertEquals(response.status_code, 404)
+
+    @override_settings(CACHES=dummy_cache_setting)
 
     def test_jpeg(self):
-        dummy_cache = cache.get_cache(
-            'django.core.cache.backends.dummy.DummyCache')
-        with patch.object(models, 'cache', dummy_cache):
-            c = Client()
-            response = c.get("{0}/{1}".format(self.url, self.jpeg.pk))
-            data = StringIO(response.content)
-            im = Image.open(data)
-            self.assertEquals(response["Content-type"], "image/jpeg")
+        c = Client()
+        response = c.get("{0}/{1}".format(self.url, self.jpeg.pk))
+        data = StringIO(response.content)
+        im = Image.open(data)
+        self.assertEquals(response["Content-type"], "image/jpeg")
 
-            orig = Image.open("%s/../resources/test_jpeg.jpg" % TEST_ROOT)
+        orig = Image.open("%s/../resources/test_jpeg.jpg" % TEST_ROOT)
 
-            self.assertEquals(im.size[0], orig.size[0])
-            self.assertEquals(im.size[1], orig.size[1])
-            self.assertEquals(im.format, 'JPEG')
+        self.assertEquals(im.size[0], orig.size[0])
+        self.assertEquals(im.size[1], orig.size[1])
+        self.assertEquals(im.format, 'JPEG')
 
+    @override_settings(CACHES=dummy_cache_setting)
     def test_gif(self):
-        dummy_cache = cache.get_cache(
-            'django.core.cache.backends.dummy.DummyCache')
-        with patch.object(models, 'cache', dummy_cache):
-            c = Client()
-            response = c.get("{0}/{1}".format(self.url, self.gif.pk))
-            data = StringIO(response.content)
-            im = Image.open(data)
-            self.assertEquals(response["Content-type"], "image/gif")
+        c = Client()
+        response = c.get("{0}/{1}".format(self.url, self.gif.pk))
+        data = StringIO(response.content)
+        im = Image.open(data)
+        self.assertEquals(response["Content-type"], "image/gif")
 
-            orig = Image.open("%s/../resources/test_gif.gif" % TEST_ROOT)
+        orig = Image.open("%s/../resources/test_gif.gif" % TEST_ROOT)
 
-            self.assertEquals(im.size[0], orig.size[0])
-            self.assertEquals(im.size[1], orig.size[1])
-            self.assertEquals(im.format, 'GIF')
+        self.assertEquals(im.size[0], orig.size[0])
+        self.assertEquals(im.size[1], orig.size[1])
+        self.assertEquals(im.format, 'GIF')
 
+    @override_settings(CACHES=dummy_cache_setting)
     def test_png(self):
-        dummy_cache = cache.get_cache(
-            'django.core.cache.backends.dummy.DummyCache')
-        with patch.object(models, 'cache', dummy_cache):
-            c = Client()
-            response = c.get("{0}/{1}".format(self.url, self.png.pk))
-            data = StringIO(response.content)
-            im = Image.open(data)
-            self.assertEquals(response["Content-type"], "image/png")
+        c = Client()
+        response = c.get("{0}/{1}".format(self.url, self.png.pk))
+        data = StringIO(response.content)
+        im = Image.open(data)
+        self.assertEquals(response["Content-type"], "image/png")
 
-            orig = Image.open("%s/../resources/test_png.png" % TEST_ROOT)
+        orig = Image.open("%s/../resources/test_png.png" % TEST_ROOT)
 
-            self.assertEquals(im.size[0], orig.size[0])
-            self.assertEquals(im.size[1], orig.size[1])
-            self.assertEquals(im.format, 'PNG')
+        self.assertEquals(im.size[0], orig.size[0])
+        self.assertEquals(im.size[1], orig.size[1])
+        self.assertEquals(im.format, 'PNG')

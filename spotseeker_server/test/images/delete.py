6045cd45a172e28c707f4eaf12424e82ddffe4d9
Sarthak Singh <ss269@uw.edu>
2020-06-30 12:23:58 -0700

    Updated file access and added media_root in test/images/delete.py

--- spotseeker_server/test/images/delete.py
+++ spotseeker_server/test/images/delete.py
@@ -12,11 +12,13 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 """
+import shutil
+import tempfile
 
 from django.test import TestCase
 from django.conf import settings
 from django.test.client import Client
-from django.core.files import File
+from django.core.files.uploadedfile import SimpleUploadedFile
 from spotseeker_server.models import Spot, SpotImage
 from os.path import abspath, dirname, isfile
 from django.test.utils import override_settings
@@ -32,208 +34,229 @@ class SpotImageDELETETest(TestCase):
     """ Tests DELETE of a SpotImage at /api/v1/spot/<spot id>/image/<image id>.
     """
     def setUp(self):
-        spot = Spot.objects.create(name="This is to test DELETEing images")
-        spot.save()
-        self.spot = spot
+        self.TEMP_DIR = tempfile.mkdtemp()
+        with self.settings(MEDIA_ROOT=self.TEMP_DIR):
+            spot = Spot.objects.create(name="This is to test DELETEing images")
+            spot.save()
+            self.spot = spot
 
-        self.url = '/api/v1/spot/{0}'.format(self.spot.pk)
-        self.url = self.url
+            self.url = '/api/v1/spot/{0}'.format(self.spot.pk)
+            self.url = self.url
 
-        # GIF
-        f = open("%s/../resources/test_gif.gif" % TEST_ROOT)
-        gif = self.spot.spotimage_set.create(
-            description="This is the GIF test",
-            image=File(f))
-        f.close()
+            # GIF
+            gif = self.spot.spotimage_set.create(
+                description="This is the GIF test",
+                image=SimpleUploadedFile(
+                    "test_gif.gif",
+                    open("%s/../resources/test_gif.gif" % TEST_ROOT).read(),
+                    'image/gif'
+                )
+            )
+            self.gif = gif
+            self.gif_url = "%s/image/%s" % (self.url, self.gif.pk)
 
-        self.gif = gif
-        self.gif_url = "%s/image/%s" % (self.url, self.gif.pk)
+            # JPEG
+            jpeg = self.spot.spotimage_set.create(
+                description="This is the JPEG test",
+                image=SimpleUploadedFile(
+                    "test_jpeg.jpg",
+                    open("%s/../resources/test_jpeg.jpg" % TEST_ROOT).read(),
+                    'image/jpeg'
+                )
+            )
+            self.jpeg = jpeg
+            self.jpeg_url = "%s/image/%s" % (self.url, self.jpeg.pk)
 
-        # JPEG
-        f = open("%s/../resources/test_jpeg.jpg" % TEST_ROOT)
-        jpeg = self.spot.spotimage_set.create(
-            description="This is the JPEG test",
-            image=File(f))
-        f.close()
+            # PNG
+            png = self.spot.spotimage_set.create(
+                description="This is the PNG test",
+                image=SimpleUploadedFile(
+                    "test_png.png",
+                    open("%s/../resources/test_png.png" % TEST_ROOT).read(),
+                    'image/png'
+                )
+            )
+            self.png = png
+            self.png_url = "%s/image/%s" % (self.url, self.png.pk)
 
-        self.jpeg = jpeg
-        self.jpeg_url = "%s/image/%s" % (self.url, self.jpeg.pk)
-
-        # PNG
-        f = open("%s/../resources/test_png.png" % TEST_ROOT)
-        png = self.spot.spotimage_set.create(
-            description="This is the PNG test",
-            image=File(f))
-        f.close()
-
-        self.png = png
-        self.png_url = "%s/image/%s" % (self.url, self.png.pk)
+    def tearDown(self):
+        shutil.rmtree(self.TEMP_DIR)
 
     def test_bad_url(self):
-        c = Client()
-        bad_url = "%s/image/aa" % self.url
-        response = c.delete(bad_url)
-        self.assertEquals(response.status_code, 404)
+        with self.settings(MEDIA_ROOT=self.TEMP_DIR):
+            c = Client()
+            bad_url = "%s/image/aa" % self.url
+            response = c.delete(bad_url)
+            self.assertEquals(response.status_code, 404)
 
     def test_wrong_spot_id(self):
-        c = Client()
-        spot = Spot.objects.create(name="This is the wrong spot")
+        with self.settings(MEDIA_ROOT=self.TEMP_DIR):
+            c = Client()
+            spot = Spot.objects.create(name="This is the wrong spot")
 
-        f = open("%s/../resources/test_png.png" % TEST_ROOT)
-        png = self.spot.spotimage_set.create(
-            description="This is another PNG", image=File(f))
-        f.close()
+            png = self.spot.spotimage_set.create(
+                description="This is another PNG",
+                image=SimpleUploadedFile(
+                    "test_png.png",
+                    open("%s/../resources/test_png.png" % TEST_ROOT).read(),
+                    'image/png'
+                )
+            )
 
-        response = \
-            c.delete("/api/v1/spot/{0}/image/{1}".format(spot.pk, png.pk))
-        self.assertEquals(response.status_code, 404)
+            response = \
+                c.delete("/api/v1/spot/{0}/image/{1}".format(spot.pk, png.pk))
+            self.assertEquals(response.status_code, 404)
 
     def test_invalid_id_too_high(self):
-        c = Client()
+        with self.settings(MEDIA_ROOT=self.TEMP_DIR):
+            c = Client()
 
-        # GIF
-        test_gif_id = self.gif.pk + 10000
-        test_url = "/api/v1/spot/%s/image/%s" % (self.url, test_gif_id)
-        response = c.delete(test_url)
-        self.assertEquals(response.status_code, 404)
+            # GIF
+            test_gif_id = self.gif.pk + 10000
+            test_url = "/api/v1/spot/%s/image/%s" % (self.url, test_gif_id)
+            response = c.delete(test_url)
+            self.assertEquals(response.status_code, 404)
 
     def test_actual_delete_with_etag(self):
-        c = Client()
+        with self.settings(MEDIA_ROOT=self.TEMP_DIR):
+            c = Client()
 
-        # GIF
-        response = c.get(self.gif_url)
-        etag = response["ETag"]
+            # GIF
+            response = c.get(self.gif_url)
+            etag = response["ETag"]
 
-        response = c.delete(self.gif_url, If_Match=etag)
+            response = c.delete(self.gif_url, If_Match=etag)
 
-        self.assertEquals(response.status_code, 200)
+            self.assertEquals(response.status_code, 200)
 
-        response = c.get(self.gif_url)
-        self.assertEquals(response.status_code, 404)
+            response = c.get(self.gif_url)
+            self.assertEquals(response.status_code, 404)
 
-        response = c.delete(self.gif_url)
-        self.assertEquals(response.status_code, 404)
+            response = c.delete(self.gif_url)
+            self.assertEquals(response.status_code, 404)
 
-        self.assertEqual(isfile(self.gif.image.path), False)
+            self.assertEqual(isfile(self.gif.image.path), False)
 
-        try:
-            test_gif = SpotImage.objects.get(pk=self.gif.pk)
-        except Exception as e:
-            test_gif = None
+            try:
+                test_gif = SpotImage.objects.get(pk=self.gif.pk)
+            except Exception as e:
+                test_gif = None
 
-        self.assertIsNone(test_gif)
+            self.assertIsNone(test_gif)
 
-        # JPEG
-        response = c.get(self.jpeg_url)
-        etag = response["ETag"]
+            # JPEG
+            response = c.get(self.jpeg_url)
+            etag = response["ETag"]
 
-        response = c.delete(self.jpeg_url, If_Match=etag)
+            response = c.delete(self.jpeg_url, If_Match=etag)
 
-        self.assertEquals(response.status_code, 200)
+            self.assertEquals(response.status_code, 200)
 
-        response = c.get(self.jpeg_url)
-        self.assertEquals(response.status_code, 404)
+            response = c.get(self.jpeg_url)
+            self.assertEquals(response.status_code, 404)
 
-        response = c.delete(self.jpeg_url)
-        self.assertEquals(response.status_code, 404)
+            response = c.delete(self.jpeg_url)
+            self.assertEquals(response.status_code, 404)
 
-        self.assertEqual(isfile(self.jpeg.image.path), False)
+            self.assertEqual(isfile(self.jpeg.image.path), False)
 
-        try:
-            test_jpeg = SpotImage.objects.get(pk=self.jpeg.pk)
-        except Exception as e:
-            test_jpeg = None
+            try:
+                test_jpeg = SpotImage.objects.get(pk=self.jpeg.pk)
+            except Exception as e:
+                test_jpeg = None
 
-        self.assertIsNone(test_jpeg)
+            self.assertIsNone(test_jpeg)
 
-        # PNG
-        response = c.get(self.png_url)
-        etag = response["ETag"]
+            # PNG
+            response = c.get(self.png_url)
+            etag = response["ETag"]
 
-        response = c.delete(self.png_url, If_Match=etag)
+            response = c.delete(self.png_url, If_Match=etag)
 
-        self.assertEquals(response.status_code, 200)
+            self.assertEquals(response.status_code, 200)
 
-        response = c.get(self.png_url)
-        self.assertEquals(response.status_code, 404)
+            response = c.get(self.png_url)
+            self.assertEquals(response.status_code, 404)
 
-        response = c.delete(self.png_url)
-        self.assertEquals(response.status_code, 404)
+            response = c.delete(self.png_url)
+            self.assertEquals(response.status_code, 404)
 
-        self.assertEqual(isfile(self.png.image.path), False)
+            self.assertEqual(isfile(self.png.image.path), False)
 
-        try:
-            test_png = SpotImage.objects.get(pk=self.png.pk)
-        except Exception as e:
-            test_png = None
+            try:
+                test_png = SpotImage.objects.get(pk=self.png.pk)
+            except Exception as e:
+                test_png = None
 
-        self.assertIsNone(test_png)
+            self.assertIsNone(test_png)
 
     def test_actual_delete_no_etag(self):
-        c = Client()
+        with self.settings(MEDIA_ROOT=self.TEMP_DIR):
+            c = Client()
 
-        # GIF
-        response = c.delete(self.gif_url)
-        self.assertEquals(response.status_code, 400)
+            # GIF
+            response = c.delete(self.gif_url)
+            self.assertEquals(response.status_code, 400)
 
-        response = c.get(self.gif_url)
-        self.assertEquals(response.status_code, 200)
+            response = c.get(self.gif_url)
+            self.assertEquals(response.status_code, 200)
 
-        # JPEG
-        response = c.delete(self.jpeg_url)
-        self.assertEquals(response.status_code, 400)
+            # JPEG
+            response = c.delete(self.jpeg_url)
+            self.assertEquals(response.status_code, 400)
 
-        response = c.get(self.jpeg_url)
-        self.assertEquals(response.status_code, 200)
+            response = c.get(self.jpeg_url)
+            self.assertEquals(response.status_code, 200)
 
-        # PNG
-        response = c.delete(self.png_url)
-        self.assertEquals(response.status_code, 400)
+            # PNG
+            response = c.delete(self.png_url)
+            self.assertEquals(response.status_code, 400)
 
-        response = c.get(self.png_url)
-        self.assertEquals(response.status_code, 200)
+            response = c.get(self.png_url)
+            self.assertEquals(response.status_code, 200)
 
     def test_actual_delete_expired_etag(self):
-        c = Client()
+        with self.settings(MEDIA_ROOT=self.TEMP_DIR):
+            c = Client()
 
-        # GIF
-        response = c.get(self.gif_url)
-        etag = response["ETag"]
+            # GIF
+            response = c.get(self.gif_url)
+            etag = response["ETag"]
 
-        intermediate_img = SpotImage.objects.get(pk=self.gif.pk)
-        intermediate_img.name = "This interferes w/ the DELETE"
-        intermediate_img.save()
+            intermediate_img = SpotImage.objects.get(pk=self.gif.pk)
+            intermediate_img.name = "This interferes w/ the DELETE"
+            intermediate_img.save()
 
-        response = c.delete(self.gif_url, If_Match=etag)
-        self.assertEquals(response.status_code, 409)
+            response = c.delete(self.gif_url, If_Match=etag)
+            self.assertEquals(response.status_code, 409)
 
-        response = c.get(self.gif_url)
-        self.assertEquals(response.status_code, 200)
+            response = c.get(self.gif_url)
+            self.assertEquals(response.status_code, 200)
 
-        # JPEG
-        response = c.get(self.jpeg_url)
-        etag = response["ETag"]
+            # JPEG
+            response = c.get(self.jpeg_url)
+            etag = response["ETag"]
 
-        intermediate_img = SpotImage.objects.get(pk=self.jpeg.pk)
-        intermediate_img.name = "This interferes w/ the DELETE"
-        intermediate_img.save()
+            intermediate_img = SpotImage.objects.get(pk=self.jpeg.pk)
+            intermediate_img.name = "This interferes w/ the DELETE"
+            intermediate_img.save()
 
-        response = c.delete(self.jpeg_url, If_Match=etag)
-        self.assertEquals(response.status_code, 409)
+            response = c.delete(self.jpeg_url, If_Match=etag)
+            self.assertEquals(response.status_code, 409)
 
-        response = c.get(self.jpeg_url)
-        self.assertEquals(response.status_code, 200)
+            response = c.get(self.jpeg_url)
+            self.assertEquals(response.status_code, 200)
 
-        # PNG
-        response = c.get(self.png_url)
-        etag = response["ETag"]
+            # PNG
+            response = c.get(self.png_url)
+            etag = response["ETag"]
 
-        intermediate_img = SpotImage.objects.get(pk=self.png.pk)
-        intermediate_img.name = "This interferes w/ the DELETE"
-        intermediate_img.save()
+            intermediate_img = SpotImage.objects.get(pk=self.png.pk)
+            intermediate_img.name = "This interferes w/ the DELETE"
+            intermediate_img.save()
 
-        response = c.delete(self.png_url, If_Match=etag)
-        self.assertEquals(response.status_code, 409)
+            response = c.delete(self.png_url, If_Match=etag)
+            self.assertEquals(response.status_code, 409)
 
-        response = c.get(self.png_url)
-        self.assertEquals(response.status_code, 200)
+            response = c.get(self.png_url)
+            self.assertEquals(response.status_code, 200)

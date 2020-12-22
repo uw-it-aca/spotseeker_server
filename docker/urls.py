dccc03080fb060083a3039bf1fd4908bd01f2386
erinRochfort99 <erochfor@uw.edu>
2020-12-22 13:04:52 -0800

    updated settings and urls for travis tests

--- docker/urls.py
+++ docker/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^auth/', include('oauth_provider.urls')),
     url(r'^api/', include('spotseeker_server.urls')),
 ]
+
+if settings.DEBUG:
+    from django.contrib import admin
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    urlpatterns += [
+        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+        url(r'^admin/', include(admin.site.urls)),
+    ]
+
+    urlpatterns += staticfiles_urlpatterns()

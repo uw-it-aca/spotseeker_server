fa3f8bb5102302cbeecf43a72a4f49322cb373b2
Craig M. Stimmel <cstimmel@uw.edu>
2021-02-16 17:18:43 -0800

    Return comment with contributed changes Start .env file as just a template to optionally use

--- spotseeker_server/models/spot.py
+++ spotseeker_server/models/spot.py
@@ -1,3 +1,28 @@
+""" Copyright 2012, 2013 UW Information Technology, University of Washington
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    Changes
+    =================================================================
+
+    sbutler1@illinois.edu: add external_id support; create decorator
+        for updating the etag; make spottypes not required; use
+        reverse() to build URLs; re-organize some of the JSON
+        serialization code; add DAY_CHOICES; add unique constraint on
+        extended info (spot, key); re-work SpotImage save so that the
+        proper exception is thrown on an invalid image type.
+"""
+
 from PIL import Image
 
 from django.contrib.auth.models import User

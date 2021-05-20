17c7e2cf8214f95f3af5fff01a9cc8124c68dc0f
Craig M. Stimmel <cstimmel@uw.edu>
2021-05-20 15:09:44 -0700

    Reformat and update copyright.

--- spotseeker_server/test/hours/post.py
+++ spotseeker_server/test/hours/post.py
@@ -1,21 +1,6 @@
 # Copyright 2021 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-""" Copyright 2012, 2013 UW Information Technology, University of Washington
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-"""
-
 from django.test import TestCase
 from django.conf import settings
 from django.test.client import Client
@@ -27,38 +12,44 @@ from spotseeker_server import models
 
 
 @override_settings(
-    SPOTSEEKER_AUTH_MODULE='spotseeker_server.auth.all_ok',
-    SPOTSEEKER_SPOT_FORM='spotseeker_server.default_forms.spot.'
-                         'DefaultSpotForm')
-@override_settings(SPOTSEEKER_AUTH_ADMINS=('demo_user',))
+    SPOTSEEKER_AUTH_MODULE="spotseeker_server.auth.all_ok",
+    SPOTSEEKER_SPOT_FORM="spotseeker_server.default_forms.spot."
+    "DefaultSpotForm",
+)
+@override_settings(SPOTSEEKER_AUTH_ADMINS=("demo_user",))
 class SpotHoursPOSTTest(TestCase):
-
     def test_hours(self):
         post_obj = {
-            'name': "This spot has available hours",
-            'capacity': 4,
-            'location': {
-                'latitude': '55',
-                'longitude': '30',
+            "name": "This spot has available hours",
+            "capacity": 4,
+            "location": {
+                "latitude": "55",
+                "longitude": "30",
             },
-            'available_hours': {
-                'monday': [["00:00", "10:00"], ["11:00", "14:00"]],
-                'tuesday': [["11:00", "14:00"]],
-                'wednesday': [["11:00", "14:00"]],
-                'thursday': [["11:00", "14:00"]],
-                'friday': [["11:00", "14:00"]],
-                'saturday': [],
-                'sunday': [["11:00", "14:00"]],
-            }
+            "available_hours": {
+                "monday": [["00:00", "10:00"], ["11:00", "14:00"]],
+                "tuesday": [["11:00", "14:00"]],
+                "wednesday": [["11:00", "14:00"]],
+                "thursday": [["11:00", "14:00"]],
+                "friday": [["11:00", "14:00"]],
+                "saturday": [],
+                "sunday": [["11:00", "14:00"]],
+            },
         }
 
         client = Client()
-        response = client.post("/api/v1/spot/", json.dumps(post_obj),
-                               content_type="application/json")
+        response = client.post(
+            "/api/v1/spot/",
+            json.dumps(post_obj),
+            content_type="application/json",
+        )
         get_response = client.get(response["Location"])
 
-        self.assertEquals(get_response.status_code, 200,
-                          "OK in response to GETing the new spot")
+        self.assertEquals(
+            get_response.status_code,
+            200,
+            "OK in response to GETing the new spot",
+        )
 
         spot_dict = json.loads(get_response.content)
 
@@ -66,15 +57,15 @@ class SpotHoursPOSTTest(TestCase):
         self.assertEquals(
             spot_dict["available_hours"],
             post_obj["available_hours"],
-            "Data from the web service matches the data for the spot"
+            "Data from the web service matches the data for the spot",
         )
         self.assertEquals(
             spot_dict["name"],
             post_obj["name"],
-            "Data from the web service matches the data for the spot"
+            "Data from the web service matches the data for the spot",
         )
         self.assertEquals(
             spot_dict["capacity"],
             post_obj["capacity"],
-            "Data from the web service matches the data for the spot"
+            "Data from the web service matches the data for the spot",
         )

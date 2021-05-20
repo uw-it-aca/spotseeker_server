9fca13fcddf9f54fabde8bef0ca8e0e4dfc7392d
Craig M. Stimmel <cstimmel@uw.edu>
2021-05-20 11:58:34 -0700

    Reformat with black.

--- spotseeker_server/org_filters/sample_search.py
+++ spotseeker_server/org_filters/sample_search.py
@@ -26,10 +26,12 @@ LOGGER = logging.getLogger(__name__)
 
 
 class Filter(SearchFilter):
-    keys = set((
-        'sample1',
-        'extended_info:sample2',
-        ))
+    keys = set(
+        (
+            "sample1",
+            "extended_info:sample2",
+        )
+    )
 
     def filter_query(self, query):
         """
@@ -39,18 +41,18 @@ class Filter(SearchFilter):
         - bar: includes baz
         """
 
-        if 'extended_info:sample2' in self.request.GET:
+        if "extended_info:sample2" in self.request.GET:
             sample = self.request.GET["extended_info:sample2"]
             if sample:
                 values = [sample]
-                if sample == 'foo':
-                    values.append('bar')
-                    values.append('baz')
-                elif sample == 'bar':
-                    values.append('baz')
+                if sample == "foo":
+                    values.append("bar")
+                    values.append("baz")
+                elif sample == "bar":
+                    values.append("baz")
                 query = query.filter(
                     spotextendedinfo__key="sample2",
-                    spotextendedinfo__value__in=values
+                    spotextendedinfo__value__in=values,
                 )
 
                 self.has_valid_search_param = True
@@ -63,7 +65,7 @@ class Filter(SearchFilter):
         and the ID is even.
         """
 
-        sample = self.request.GET.get('sample1', '')
+        sample = self.request.GET.get("sample1", "")
         if not sample:
             result = spots
         else:

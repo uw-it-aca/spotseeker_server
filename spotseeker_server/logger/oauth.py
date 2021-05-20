f45ea1cf57246553005623362ab7e621b823d525
Craig M. Stimmel <cstimmel@uw.edu>
2021-05-20 15:28:39 -0700

    Reformat and update copyright.

--- spotseeker_server/logger/oauth.py
+++ spotseeker_server/logger/oauth.py
@@ -1,22 +1,7 @@
 # Copyright 2021 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-""" Copyright 2012, 2013 UW Information Technology, University of Washington
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-
-
-    This creates a log along the lines of the apache
+""" This creates a log along the lines of the apache
     common log format, but with info about the OAuth apps.
     This is the format:
 
@@ -36,14 +21,14 @@ class LogMiddleware(MiddlewareMixin):
 
     def process_response(self, request, response):
         request_uri = request.get_full_path()
-        request_method = request.META['REQUEST_METHOD']
+        request_method = request.META["REQUEST_METHOD"]
 
         response_status = response.status_code
 
         # response.content will empty out the FileWrapper object
         # on file downloads - those views need to correctly set
         # their own content length
-        if 'Content-Length' in response:
+        if "Content-Length" in response:
             response_length = response["Content-Length"]
         else:
             response_length = len(response.content)
@@ -61,10 +46,16 @@ class LogMiddleware(MiddlewareMixin):
 
         timestamp = datetime.now().strftime("%d/%b/%Y %H:%M:%S")
 
-        log_message = "[{0}] {1}\t\"{2}\"\t{3}\t\"{4} {5}\" "\
-            "{6} {7}".format(timestamp, oauth_app_pk, oauth_app_name,
-                             oauth_user, request_method, request_uri,
-                             response_status, response_length)
+        log_message = '[{0}] {1}\t"{2}"\t{3}\t"{4} {5}" ' "{6} {7}".format(
+            timestamp,
+            oauth_app_pk,
+            oauth_app_name,
+            oauth_user,
+            request_method,
+            request_uri,
+            response_status,
+            response_length,
+        )
 
         self.logger.info(log_message)
 

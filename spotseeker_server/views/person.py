a75aacf9d5e41adeda17ce4c5245c5340c2a1848
Craig M. Stimmel <cstimmel@uw.edu>
2021-05-20 12:53:20 -0700

    Reformat and update copyright.

--- spotseeker_server/views/person.py
+++ spotseeker_server/views/person.py
@@ -1,29 +1,14 @@
 # Copyright 2021 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-""" Copyright 2014 UW Information Technology, University of Washington
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-
-"""
-
 from spotseeker_server.views.rest_dispatch import RESTDispatch, JSONResponse
 from spotseeker_server.require_auth import user_auth_required
 from django.conf import settings
 
 
 class PersonView(RESTDispatch):
-    """ Information (username, email) about a person """
+    """Information (username, email) about a person"""
+
     @user_auth_required
     def GET(self, request):
         user = self._get_user(request)

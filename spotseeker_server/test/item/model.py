dee9309aae20bc99049b7b12512db099d39cece6
Craig M. Stimmel <cstimmel@uw.edu>
2021-05-20 14:56:26 -0700

    Reformat and update copyright.

--- spotseeker_server/test/item/model.py
+++ spotseeker_server/test/item/model.py
@@ -1,20 +1,6 @@
 # Copyright 2021 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-""" Copyright 2012, 2013 UW Information Technology, University of Washington
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-"""
 from builtins import range
 
 from django.conf import settings
@@ -27,34 +13,35 @@ from spotseeker_server.models import Spot, Item, ItemExtendedInfo
 
 
 @override_settings(
-    SPOTSEEKER_AUTH_MODULE='spotseeker_server.auth.all_ok',
-    SPOTSEEKER_SPOT_FORM='spotseeker_server.default_forms.spot.'
-                         'DefaultSpotForm')
+    SPOTSEEKER_AUTH_MODULE="spotseeker_server.auth.all_ok",
+    SPOTSEEKER_SPOT_FORM="spotseeker_server.default_forms.spot."
+    "DefaultSpotForm",
+)
 class ItemModelTest(TestCase):
-    """ Tests Item model data relations and retrieval. """
+    """Tests Item model data relations and retrieval."""
 
     def setUp(self):
         # create a Spot
-        self.spot = Spot.objects.create(name='spot %s' % randstring())
+        self.spot = Spot.objects.create(name="spot %s" % randstring())
         self.spot_id = self.spot.pk
 
         # Item category and subcategory
-        self.category = 'Stuff'
-        self.subcategory = 'Thing'
+        self.category = "Stuff"
+        self.subcategory = "Thing"
 
         # create an item
         self.checkout_item = Item.objects.create(
-            name='an item',
+            name="an item",
             spot=self.spot,
             item_category=self.category,
-            item_subcategory=self.subcategory)
+            item_subcategory=self.subcategory,
+        )
 
         # create 4 items extended info
         for i in range(1, 5):
             ItemExtendedInfo.objects.create(
-                item=self.checkout_item,
-                key='key %s' % i,
-                value='value %s' % i)
+                item=self.checkout_item, key="key %s" % i, value="value %s" % i
+            )
 
     def tearDown(self):
         # Deleting the spot cascade deletes the item and IEI
@@ -64,23 +51,23 @@ class ItemModelTest(TestCase):
         # get the Spot json
         test_spot = Spot.objects.get(pk=self.spot_id)
         json_data = test_spot.json_data_structure()
-        self.assertIn('items', json_data)
-        for item in json_data['items']:
+        self.assertIn("items", json_data)
+        for item in json_data["items"]:
             # assert that the Spot json contains the Item
-            self.assertIn('name', item)
-            self.assertEqual(item['name'], self.checkout_item.name)
+            self.assertIn("name", item)
+            self.assertEqual(item["name"], self.checkout_item.name)
 
             # assert Item category and subcategory
-            self.assertEqual(item['category'], self.category)
-            self.assertEqual(item['subcategory'], self.subcategory)
-            self.assertIn('extended_info', item)
+            self.assertEqual(item["category"], self.category)
+            self.assertEqual(item["subcategory"], self.subcategory)
+            self.assertIn("extended_info", item)
 
             prev_key = None
             prev_value = None
 
-            for ei_key, ei_value in item['extended_info'].items():
-                self.assertIn('key ', ei_key)
-                self.assertIn('value ', ei_value)
+            for ei_key, ei_value in item["extended_info"].items():
+                self.assertIn("key ", ei_key)
+                self.assertIn("value ", ei_value)
                 # Also make sure each one is unique
                 self.assertNotEqual(ei_key, prev_key)
                 self.assertNotEqual(ei_value, prev_value)
@@ -90,4 +77,4 @@ class ItemModelTest(TestCase):
 
 
 def randstring():
-    return ''.join(random.choice(ascii_lowercase) for n in range(25))
+    return "".join(random.choice(ascii_lowercase) for n in range(25))

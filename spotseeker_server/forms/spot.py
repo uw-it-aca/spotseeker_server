bcc319833617ab12752b1b57dc6b1cb605e36a47
Craig M. Stimmel <cstimmel@uw.edu>
2021-05-20 13:36:50 -0700

    Reformat and update copyright.

--- spotseeker_server/forms/spot.py
+++ spotseeker_server/forms/spot.py
@@ -1,21 +1,7 @@
 # Copyright 2021 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-""" Copyright 2012, 2013 UW Information Technology, University of Washington
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-
-    Changes
+""" Changes
     =================================================================
 
     sbutler1@illinois.edu: load the forms on application load, not every
@@ -26,16 +12,18 @@
     breaks our unit tests.
 """
 
-from spotseeker_server.default_forms.spot import \
-    DefaultSpotForm, DefaultSpotExtendedInfoForm
+from spotseeker_server.default_forms.spot import (
+    DefaultSpotForm,
+    DefaultSpotExtendedInfoForm,
+)
 from spotseeker_server.load_module import ModuleObjectLoader
 
 
 class SpotExtendedInfoForm(ModuleObjectLoader):
-    setting_name = 'SPOTSEEKER_SPOTEXTENDEDINFO_FORM'
+    setting_name = "SPOTSEEKER_SPOTEXTENDEDINFO_FORM"
     default = DefaultSpotExtendedInfoForm
 
 
 class SpotForm(ModuleObjectLoader):
-    setting_name = 'SPOTSEEKER_SPOT_FORM'
+    setting_name = "SPOTSEEKER_SPOT_FORM"
     default = DefaultSpotForm

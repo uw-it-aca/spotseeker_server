9e97981e97b2c1f336e658bbd14bac80e9208ce0
Craig M. Stimmel <cstimmel@uw.edu>
2021-05-20 13:02:49 -0700

    Reformat and update copyright.

--- spotseeker_server/views/null.py
+++ spotseeker_server/views/null.py
@@ -1,22 +1,7 @@
 # Copyright 2021 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-""" Copyright 2012, 2013 UW Information Technology, University of Washington
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-
-
-This class is just here to help with testing coverage
+""" This class is just here to help with testing coverage
 """
 from spotseeker_server.views.rest_dispatch import RESTDispatch
 

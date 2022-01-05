2ab8bfb920660bd2956e066e23683b03ef25a218
Craig M. Stimmel <cstimmel@uw.edu>
2022-01-05 15:21:52 -0800

    Update copyright date on UW files

--- spotseeker_server/auth/oauth.py
+++ spotseeker_server/auth/oauth.py
@@ -1,4 +1,4 @@
-# Copyright 2021 UW-IT, University of Washington
+# Copyright 2022 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
 """ This module uses oauth to allow applications and users access.
--- spotseeker_server/management/commands/create_consumer.py
+++ spotseeker_server/management/commands/create_consumer.py
@@ -1,4 +1,4 @@
-# Copyright 2021 UW-IT, University of Washington
+# Copyright 2022 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
 """ This provides a management command to django's manage.py called

754ebaa0efc608c669f8953b6785015c5cdb8e3c
Craig M. Stimmel <cstimmel@uw.edu>
2022-06-01 15:52:53 -0700

    typo

--- spotseeker_server/management/commands/techloan/techloan.py
+++ spotseeker_server/management/commands/techloan/techloan.py
@@ -57,7 +57,7 @@ class Techloan:
 
     @classmethod
     def from_cte_api(cls) -> 'Techloan':
-        loggger.debug("getting data from CTE api")
+        logger.debug("getting data from CTE api")
         if cls._url is None:
             raise ImproperlyConfigured("No techloan URL configured")
         equipments = requests.get(cls._url).json()

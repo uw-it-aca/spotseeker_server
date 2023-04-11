c351cea2be862652cc8b2e17ba875a3865915df1
David-Strupinski <dstrupin@uw.edu>
2023-04-11 14:27:39 -0700

    update copyright year

--- spotseeker_server/forms/item.py
+++ spotseeker_server/forms/item.py
@@ -1,4 +1,4 @@
-# Copyright 2022 UW-IT, University of Washington
+# Copyright 2023 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
 from spotseeker_server.default_forms.item import (

7005c13aad0574fb5dc37ff8eb79a629d733a819
David-Strupinski <dstrupin@uw.edu>
2023-04-20 16:56:09 -0700

    add oauth2 endpoint views

--- spotseeker_server/urls.py
+++ spotseeker_server/urls.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from django.urls import path, re_path, include
+from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from spotseeker_server.views.buildings import BuildingListView
 from spotseeker_server.views.spot import SpotView
@@ -16,6 +17,66 @@ from spotseeker_server.views.person import PersonView
 from spotseeker_server.views.item_image import ItemImageView
 from spotseeker_server.views.add_item_image import AddItemImageView
 from spotseeker_server.views.item_thumbnail import ItemThumbnailView
+import oauth2_provider.views as oauth2_views
+
+# OAuth2 provider endpoints
+oauth2_endpoint_views = [
+    path(
+        "authorize/",
+        oauth2_views.AuthorizationView.as_view(),
+        name="authorize"
+    ),
+    path("token/", oauth2_views.TokenView.as_view(), name="token"),
+    path(
+        "revoke-token/",
+        oauth2_views.RevokeTokenView.as_view(),
+        name="revoke-token"
+    ),
+]
+
+if settings.DEBUG:
+    # OAuth2 Application Management endpoints
+    oauth2_endpoint_views += [
+        path(
+            "applications/",
+            oauth2_views.ApplicationList.as_view(),
+            name="list"
+        ),
+        path(
+            "applications/register/",
+            oauth2_views.ApplicationRegistration.as_view(),
+            name="register"
+        ),
+        path(
+            "applications/<int:pk>/",
+            oauth2_views.ApplicationDetail.as_view(),
+            name="detail"
+        ),
+        path(
+            "applications/<int:pk>/delete/",
+            oauth2_views.ApplicationDelete.as_view(),
+            name="delete"
+        ),
+        path(
+            "applications/<int:pk>/update/",
+            oauth2_views.ApplicationUpdate.as_view(),
+            name="update"
+        ),
+    ]
+
+    # OAuth2 Token Management endpoints
+    oauth2_endpoint_views += [
+        path(
+            "authorized-tokens/",
+            oauth2_views.AuthorizedTokensListView.as_view(),
+            name="authorized-token-list"
+        ),
+        path(
+            "authorized-tokens/<int:pk>/delete/",
+            oauth2_views.AuthorizedTokenDeleteView.as_view(),
+            name="authorized-token-delete"
+        ),
+    ]
 
 urlpatterns = [
     path("v1/null", csrf_exempt(NullView().run)),

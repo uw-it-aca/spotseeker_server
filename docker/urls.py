fcc63b5bfb9f31e7b8df49105973f2386e8d74f2
David-Strupinski <dstrupin@uw.edu>
2023-04-20 16:10:12 -0700

    move admin site out of debug

--- docker/urls.py
+++ docker/urls.py
@@ -1,20 +1,9 @@
-"""docker URL Configuration
+# Copyright 2023 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.urls import path, re_path, include
+from django.contrib import admin
 
 urlpatterns = [
     path('api/', include('spotseeker_server.urls')),
@@ -25,7 +14,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    from django.contrib import admin
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     urlpatterns += [

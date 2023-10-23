af16e33f71f7b3234586f62886ba36bb0e855020
Craig M. Stimmel <cstimmel@uw.edu>
2023-10-23 16:28:35 -0700

    Fix metrics url 404s

--- docker/urls.py
+++ docker/urls.py
@@ -1,15 +1,15 @@
 # Copyright 2023 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
+from .base_urls import *
 from django.conf import settings
 from django.urls import path, include
 from django.contrib import admin
 
-urlpatterns = [
-    path('api/', include('spotseeker_server.urls')),
+urlpatterns += [
+    path("api/", include("spotseeker_server.urls")),
     path(
-        'auth/',
-        include('oauth2_provider.urls', namespace='oauth2_provider')
+        "auth/", include("oauth2_provider.urls", namespace="oauth2_provider")
     ),
 ]
 
@@ -17,8 +17,8 @@ if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     urlpatterns += [
-        path('admin/doc/', include('django.contrib.admindocs.urls')),
-        path('admin/', admin.site.urls),
+        path("admin/doc/", include("django.contrib.admindocs.urls")),
+        path("admin/", admin.site.urls),
     ]
 
     urlpatterns += staticfiles_urlpatterns()
